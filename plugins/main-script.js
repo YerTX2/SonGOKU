388e4f8fe616aefd1fa717b04c91342c9ab671a6
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-07-08 19:13:35 -0500

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/DanielDiod/Sumi-Sakurasawa')
+let res = await fetch('https://api.github.com/repos/YerTX2/GokuBlackRose')
 let json = await res.json()
 try {
 let txt = '`- 乂  S C R I P T  -  M A I N`\n\n'
