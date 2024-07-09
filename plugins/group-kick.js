7d65db15ea71651e20bcc90bb61fb12643b13755
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-07-09 05:34:02 -0500

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -6,7 +6,7 @@ if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`🍭 Usuario eliminado.`)
+m.reply(`✅ Usuario eliminado.`)
 }
 
 handler.help = ['kick @user']
