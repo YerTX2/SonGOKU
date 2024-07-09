eb0c804579434b7990162a180899fb6b6f0690ed
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-07-09 06:54:45 -0500

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -3,13 +3,13 @@ import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: '🍭 Este comando solo puede ser utilizado en el bot principal.'}, {quoted: m});
+    return conn.sendMessage(m.chat, {text: '⚠️ Este comando solo puede ser utilizado en el bot principal.'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: '🍭 Iniciando...'}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: '✅ Iniciando...'}, {quoted: m});
   const sessionPath = './sessions'
   try {
     if (!existsSync(sessionPath)) {
-      return await conn.sendMessage(m.chat, {text: '🍭 La carpeta sessions no existe o está vacía.'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '⚠️ La carpeta sessions no existe o está vacía.'}, {quoted: m});
     }
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
@@ -20,12 +20,12 @@ const handler = async (m, { conn, usedPrefix }) => {
       }
     }
     if (filesDeleted === 0) {
-      await conn.sendMessage(m.chat, {text: '🍭 No se encontró ningún archivo para eliminar en la carpeta sessions.'}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: '⚠️ No se encontró ningún archivo para eliminar en la carpeta sessions.'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `🍭 Se eliminaron ${filesDeleted} archivos.`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `✅ Se eliminaron ${filesDeleted} archivos.`}, {quoted: m});
     }
   } catch {
-    await conn.sendMessage(m.chat, {text: '🍭 Ocurrió un error al eliminar los archivos de sesión.'}, {quoted: m});
+    await conn.sendMessage(m.chat, {text: '⚠️ Ocurrió un error al eliminar los archivos de sesión.'}, {quoted: m});
   }
   await conn.sendMessage(m.chat, {text: `¡Hola! ¿Ahora me ves?`}, {quoted: m});
 };
