0c1394f2c18350ac493af9105cabe59b06cf4f91
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-07-10 01:57:58 -0500

    Add files via upload

--- plugins/nsfw-hentai.js
+++ plugins/nsfw-hentai.js
@@ -1,20 +1,20 @@
-import Starlights from "@StarlightsTeam/Scraper"
+import { googleImage, pinterest } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!global.db.data.chats[m.chat].nsfw) return conn.reply(m.chat, `🚩 El grupo no admite contenido *Nsfw.*\n\n> Para activarlo un *Administrador* debe usar el comando */nsfw on*`, m, rcanal)
-if (!text) return m.reply('🚩 Ingresa el nombre de la imágen que estas buscando.')
+
 await m.react('🕓')
 try {
-let { dl_url } = await Starlights.rule34(text)
-await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*» Resultado* : ${text}`, m, null, rcanal)
+let res = await (await googleImage('Imagen ' + 'hentai')).getRandom()
+await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', `*» Hentai*`, m, null, rcanal)
 await m.react('✅')
 } catch {
 await m.react('✖️')
 }}
-handler.help = ['rule34 *<búsqueda>*']
+handler.help = ['hentai']
 handler.tags = ['nsfw']
-handler.command = ['rule34', 'r34']
-handler.register = true 
-//handler.limit = 20
+handler.command = ['hentai']
 handler.group = true 
+handler.register = true
+//handler.limit = 10
 export default handler
