49bc6aa9f159deed98bce47f12ab7d154b84e7fb
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 13:43:12 -0500

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
+let pp = 'https://telegra.ph/file/8ae4c2700f363fe3b6fd4.mp4'
 await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*', mentions: [m.sender] }, { quoted: m })
 await conn.groupLeave(id)}
 handler.help = ['salir']
