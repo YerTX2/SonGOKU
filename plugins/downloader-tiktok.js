e36c4f7982b277ae2bc009f0d7ac82c14c51e8bb
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 13:30:26 -0500

    Actualizar downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0])  m.reply(`🍭 Ingresa un enlace del vídeo de TikTok junto al comando.\n\nEjemplo:\n${usedPrefix + command} https://vm.tiktok.com/ZMMCYHnxf/`)
+    if (!args[0])  m.reply(`🔍 Ingresa un enlace del vídeo de TikTok junto al comando.\n\nEjemplo:\n${usedPrefix + command} https://vm.tiktok.com/ZMMCYHnxf/`)
 
     try {
         let { title, published, quality, likes, commentCount, shareCount, views, dl_url } = await Scraper.tiktokdl(args[0])
             let txt = `╭─⬣「 *TikTok Download* 」⬣\n`
-                txt += `│  ≡◦ *🍭 Título* : ${title}\n`
+                txt += `│  ≡◦ *🔍 Título* : ${title}\n`
                 txt += `│  ≡◦ *📅 Publicado* : ${published}\n`
                 txt += `│  ≡◦ *🪴 Calidad* : ${quality}\n`
                 txt += `│  ≡◦ *👍 Likes* : ${likes}\n`
@@ -26,13 +26,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         if (data.status) {
             const { author, view, comment, play, share, download, duration, title, video } = data.data;
             let txt = `╭─⬣「 *TikTok Download* 」⬣\n`
-                txt += `│  ≡◦ *🍭 Título* : ${title}\n`
+                txt += `│  ≡◦ *🔍 Título* : ${title}\n`
                 txt += `│  ≡◦ *📚 Autor* : ${author.nickname}\n`
                 txt += `│  ≡◦ *🕜 Duración* : ${duration} Segundos\n`
                 txt += `│  ≡◦ *🌵 Descargas* : ${download}\n`
                 txt += `│  ≡◦ *🗣 Comentarios* : ${comment}\n`
                 txt += `│  ≡◦ *💫 Share* : ${share}\n`
-                txt += `│  ≡◦ *🐢 Visitas* : ${play}\n`
+                txt += `│  ≡◦ *🌀 Visitas* : ${play}\n`
                 txt += `╰─⬣`
 
             await conn.sendMessage(m.chat, { video: { url: video }, caption: txt }, { quoted: m })
@@ -48,8 +48,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
             const fileSize = convertBytesToMB(meta.media[0].size_org)
 
             let txt = `╭─⬣「 *TikTok Download* 」⬣\n`
-                txt += `│  ≡◦ *🍭 Título* : ${title}\n`
-                txt += `│  ≡◦ *🐢 Autor* : ${author.nickname}\n`
+                txt += `│  ≡◦ *🔍 Título* : ${title}\n`
+                txt += `│  ≡◦ *👥 Autor* : ${author.nickname}\n`
                 txt += `│  ≡◦ *🕜 Duración* : ${duration} Segundos\n`
                 txt += `│  ≡◦ *📹 Reproducciones* : ${repro}\n`
                 txt += `│  ≡◦ *👍 Likes* : ${like}\n`;
