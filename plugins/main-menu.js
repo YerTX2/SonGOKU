a00a723bf32cea1d8a92bfe0d93c03b825af4a99
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 01:24:43 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -147,33 +147,13 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     
-    //await conn.sendFile(m.chat, fantasyImg, 'lp.jpg', menu, fake0, false, { contextInfo: {mentionedJid, externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 𝙉𝙖𝙩𝙨𝙪𝙠𝙞𝘽𝙤𝙩 ', previewType: 0, thumbnail: picture, sourceUrl: nn }}})
-//await conn.sendFile(m.chat, gataVidMenu.getRandom(), 'gata.mp4', menu, fake0)
+   
+let img = await (await fetch(`https://telegra.ph/file/e9eb6e019e4cec6e564c5.mp4`)).buffer()
+    await m.react('⭐')
+   // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
+    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
+   //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo) 
 
-const natsuki = ['https://telegra.ph/file/cbfe9e51c144cd2bf11f3.mp4',
-'https://telegra.ph/file/e9eb6e019e4cec6e564c5.mp4',
-'https://telegra.ph/file/d9942e847d6a2f7f26434.mp4']
-
-/*await conn.sendMessage(m.chat, { video: { url: natsuki.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fake0 })*/
- 
-await conn.sendMessage(m.chat, { video: { url: natsuki.getRandom() }, gifPlayback: true, caption: menu,
-contextInfo: {
-isForwarded: true,
-forwardedNewsletterMessageInfo: {
-newsletterJid: '120363314192605628@newsletter',
-newsletterName: "[🐉SonGOKU-MD🐉]",
-serverMessageId: -1
-},
-forwardingScore: 999,
-externalAdReply: {
-mentionedJid: [m.sender],
-title: gt,
-body: wm,
-thumbnailUrl: pp,
-sourceUrl: md,
-mediaType: 1,
-renderLargerThumbnail: false
-}}})
 
   } catch (e) {
     conn.reply(m.chat, 'Lo sentimos, el menú tiene un error.', m)
