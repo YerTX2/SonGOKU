845ce592dc82dc8177516637c2fcc6eabdf41eb7
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 01:33:45 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -150,7 +150,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
    
 let img = await (await fetch(`https://telegra.ph/file/e9eb6e019e4cec6e564c5.mp4`)).buffer()
     await m.react('⭐')
-   // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
+   
+/*await conn.sendMessage(m.chat, { video: { url: natsuki.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fake0 })*/
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
    //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo) 
 
