bd40e6405ee207c205ffcc7c33e241ae334bfcbe
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 14:08:40 -0500

    Actualizar group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('🍭 Bot activo en este grupo.')   
+    m.reply('✅ Bot activo en este grupo.')   
 }
 handler.help = ['desbanearbot']
 handler.tags = ['group']
