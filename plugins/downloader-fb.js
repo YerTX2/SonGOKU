d721e36a060298f4aa10b1d29c7a3caaed4ff86e
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 13:47:34 -0500

    Actualizar downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,11 +1,11 @@
 import Scraper from '@SumiFX/Scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply('🍭 Ingresa el enlace del vídeo de FaceBook junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.facebook.com/official.trash.gang/videos/873759786348039/?mibextid=rS40aB7S9Ucbxw6v`)
+if (!args[0]) return m.reply('🔍 Ingresa el enlace del vídeo de FaceBook junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.facebook.com/official.trash.gang/videos/873759786348039/?mibextid=rS40aB7S9Ucbxw6v`)
 
 try {
 let { title, SD, HD } = await Scraper.fbdl(args[0])
-await conn.sendMessage(m.chat, { video: { url: SD || HD }, caption: `*🍭 Titulo ∙* ${title}` }, { quoted: m})
+await conn.sendMessage(m.chat, { video: { url: SD || HD }, caption: `*🔍 Titulo ∙* ${title}` }, { quoted: m})
 } catch {
 }}
 handler.help = ['facebook <url fb>']
