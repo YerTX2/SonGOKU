df3783ac257b46ac4b9e4a97419d210d8b0c24a6
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 14:03:51 -0500

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('🍭 Chat Baneado con exito.')
+    m.reply('🌀 Chat Baneado con exito.')
 }
 handler.help = ['banearbot']
 handler.tags = ['group']
