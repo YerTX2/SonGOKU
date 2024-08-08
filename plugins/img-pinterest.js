9d75288b0daab87889c5a9b30c0cf358d0fcffd8
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 14:11:30 -0500

    Actualizar img-pinterest.js

--- plugins/img-pinterest.js
+++ plugins/img-pinterest.js
@@ -1,7 +1,7 @@
 import Scraper from "@SumiFX/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('🍭 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Sumi Sakurasawa Icons`)
+if (!text) return m.reply('🔍 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Sumi Sakurasawa Icons`)
 try {
 let { dl_url } = await Scraper.pinterest(text)
 await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', null, m)
