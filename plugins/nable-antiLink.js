a3eca639e2147ee49a4a134c1564be9d157061c6
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 14:13:47 -0500

    Actualizar nable-antiLink.js

--- plugins/nable-antiLink.js
+++ plugins/nable-antiLink.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
    if (args[0] === 'on') {
       if (chat.antiLink) return conn.reply(m.chat, 'antiLink ya esta Activado.', m)
       chat.antiLink = true
-      await conn.reply(m.chat, '🍭 Anti-Link activado para este Grupo.', m)
+      await conn.reply(m.chat, '✅ Anti-Link activado para este Grupo.', m)
    } else if (args[0] === 'off') {
       if (!chat.antiLink) return conn.reply(m.chat, 'Anti-Link ya esta Desactivado.', m)
       chat.antiLink = false
-      await conn.reply(m.chat, '🍭 Anti-Link desactivado para este Grupo.', m)
+      await conn.reply(m.chat, '❌ Anti-Link desactivado para este Grupo.', m)
    } else {
       await conn.reply(m.chat, `*Configurar Anti-Link*. Escriba on para activar y off para Desactivar.`, m)
    }
