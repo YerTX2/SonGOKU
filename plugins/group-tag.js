2f01be6f959eb693725e7cfe03e62f6b6ecfe7ec
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 14:06:39 -0500

    Actualizar group-tag.js

--- plugins/group-tag.js
+++ plugins/group-tag.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants}) => {
 let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-if (!m.quoted) return m.reply(`🍭 Responde a un mensaje.`)
+if (!m.quoted) return m.reply(`🌀 Responde a un mensaje.`)
 conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users } )
 }
 handler.help = ['tag']
