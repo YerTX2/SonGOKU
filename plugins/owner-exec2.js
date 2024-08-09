b837b79aaf960eaa7f5e3ac3c6cec2d92a51a881
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-09 15:00:38 -0500

    Actualizar owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -3,7 +3,7 @@ import { promisify } from 'util'
 let exec = promisify(_exec).bind(cp)
 let handler = async (m, { conn, isOwner, command, text }) => {
   if (conn.user.jid != conn.user.jid) return
-  m.reply('🍭 Ejecutando...')
+  m.reply('✅ Ejecutando...')
   let o
   try {
     o = await exec(command.trimStart()  + ' ' + text.trimEnd())
