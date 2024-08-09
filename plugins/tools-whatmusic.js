e01b51cca625519e75ff19009398c7432f79c23c
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-09 15:05:07 -0500

    Actualizar tools-whatmusic.js

--- plugins/tools-whatmusic.js
+++ plugins/tools-whatmusic.js
@@ -14,12 +14,12 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (status.code !== 0) throw status.msg 
   let { title, artists, album, genres, release_date } = metadata.music[0]
   let txt = '╭─⬣「 *Whatmusic Tools* 」⬣\n'
-      txt += `│  ≡◦ *🍭 Titulo ∙* ${title}${artists ? `\n│  ≡◦ *👤 Artista ∙* ${artists.map(v => v.name).join(', ')}` : ''}`
+      txt += `│  ≡◦ *🔍 Titulo ∙* ${title}${artists ? `\n│  ≡◦ *👤 Artista ∙* ${artists.map(v => v.name).join(', ')}` : ''}`
       txt += `${album ? `\n│  ≡◦ *📚 Album ∙* ${album.name}` : ''}${genres ? `\n│  ≡◦ *🪴 Genero ∙* ${genres.map(v => v.name).join(', ')}` : ''}\n`
       txt += `│  ≡◦ *🕜 Fecha de lanzamiento ∙* ${release_date}\n`
       txt += `╰─⬣`
      conn.reply(m.chat, txt, m)
-  } else return conn.reply(m.chat, `🍭 Etiqueta un audio o video de poca duración con el comando *${usedPrefix + command}* para ver que música contiene.`, m)
+  } else return conn.reply(m.chat, `🌀 Etiqueta un audio o video de poca duración con el comando *${usedPrefix + command}* para ver que música contiene.`, m)
 }
 handler.help = ['whatmusic <audio/video>']
 handler.tags = ['tools']
