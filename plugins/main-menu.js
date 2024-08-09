1ac41550451ae1ee5572996be3735303126810b2
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-08 22:54:43 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -8,7 +8,7 @@ let tags = {
   'search': 'Busquedas 🔎',
   'game': 'Juegos 🎮',
   'serbot': 'Sub Bots 🤖',
-  'rpg': 'RPG',
+ 'rpg': 'RPG 🌠',
   'rg': 'Registro 📁',
   'sticker': 'Stickers 🏞',
   'img': 'Imágenes 📸',
