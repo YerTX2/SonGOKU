105a465627e8c4bfc673deb8c1f89e110dbde862
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-09 15:01:20 -0500

    Actualizar tools-tomp3.js

--- plugins/tools-tomp3.js
+++ plugins/tools-tomp3.js
@@ -3,7 +3,7 @@ import { toAudio } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-if (!/video|audio/.test(mime)) throw `🍭 Responde al *Video* o *Nota de Voz* que desea convertir a mp3.`
+if (!/video|audio/.test(mime)) throw `✅ Responde al *Video* o *Nota de Voz* que desea convertir a mp3.`
 try {
 let media = await q.download?.()
 if (!media) return null
