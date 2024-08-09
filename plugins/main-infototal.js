09d5c2bab4b476f825a887c6e82533fbb07c5b2d
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-09 15:04:43 -0500

    Actualizar main-infototal.js

--- plugins/main-infototal.js
+++ plugins/main-infototal.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `*🍭 Total de Funciones* : ${totalf}`,m)
+conn.reply(m.chat, `*✅ Total de Funciones* : ${totalf}`,m)
 }
 
 handler.help = ['totalfunciones']
