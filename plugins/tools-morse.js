8293fe1956c52681555b32c6a6af08951d60411b
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-09 15:05:58 -0500

    Actualizar tools-morse.js

--- plugins/tools-morse.js
+++ plugins/tools-morse.js
@@ -122,7 +122,7 @@ let handler = async(m, { conn, usedPrefix, command, text }) => {
     }
     m.reply(Output_String)
   } else {
-    m.reply(`🍭 Ingresa un texto mas el comando.\n\nEjemplo\nEncode: *${usedPrefix}${command}* encode Hola mundo\nDecode : *${usedPrefix}${command}* decode •••• • •-•• •-•• --- / •-- --- •-• •-•• -••`)
+    m.reply(`🔍 Ingresa un texto mas el comando.\n\nEjemplo\nEncode: *${usedPrefix}${command}* encode Hola mundo\nDecode : *${usedPrefix}${command}* decode •••• • •-•• •-•• --- / •-- --- •-• •-•• -••`)
   }
 }
 
