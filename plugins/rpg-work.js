cc0c042be5113910d27a910e96250cb11972ea55
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-10 10:04:22 -0500

    Add files via upload

--- plugins/rpg-work.js
+++ plugins/rpg-work.js
@@ -1,65 +1,44 @@
-const cooldowns = {}
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-   let user = global.db.data.users[m.sender]
-   let amount = Math.floor(Math.random() * (5 - 10) + 10) + 1
-   const tiempoEspera = 5 * 60 // 5 minutos
-  if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
-    const tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    m.reply(`🕜 Espera *${tiempoRestante}* para volver a Trabajar.`)
-    return
-  }
-   let work = works.getRandom()
-   user.limit += amount
-   await m.reply(`${work} *${amount} 🍬 Dulces.*`)
-   cooldowns[m.sender] = Date.now()
+
+const handler = async (m, { conn, isPrems }) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.rpg_work
+
+  global.work = tradutor.texto1;
+
+
+  let enviando;
+  if (enviando) return
+  enviando = true
+  const hasil = Math.floor(Math.random() * 5000);
+  const time = global.db.data.users[m.sender].lastwork + 600000;
+  if (new Date - global.db.data.users[m.sender].lastwork < 600000) throw `⚔️ *¡Espera un momento pequeño aventurero!* ⚔️\n\n*—◉ Regresa a la travesía en ${msToTime(time - new Date())} ⏳*`;
+  conn.sendMessage(m.chat, { text: `🏞️ *Te embarcas en una emocionante aventura:*\n\n🛠️ *${pickRandom(global.work)}*\n\n*¡Ganaste ${hasil} exp por tu valentía!*` }, { quoted: m });
+  global.db.data.users[m.sender].exp += hasil;
+  global.db.data.users[m.sender].lastwork = new Date() * 1;
+  enviando = false
+};
+handler.help = ['work'];
+handler.tags = ['xp'];
+handler.command = /^(work|trabajar|chambear)$/i
+handler.fail = null;
+export default handler;
+
+function msToTime(duration) {
+  const milliseconds = parseInt((duration % 1000) / 100);
+  let seconds = Math.floor((duration / 1000) % 60);
+  let minutes = Math.floor((duration / (1000 * 60)) % 60);
+  let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+  hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
+  return minutes + ' minutos ' + seconds + ' segundos ';
 }
 
-handler.help = ['work']
-handler.tags = ['rpg']
-handler.command = ['work', 'trabajar', 'w']
-handler.register = true 
-export default handler
-
-function segundosAHMS(segundos) {
-  const minutos = Math.floor((segundos % 3600) / 60)
-  const segundosRestantes = segundos % 60
-  return `${minutos} minutos y ${segundosRestantes} segundos`
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())];
 }
 
-// Thanks to FG98
-const works = [
-   "Trabajas como cortador de galletas y ganas",
-   "Trabaja para una empresa militar privada, ganando",
-   "Organiza un evento de cata de vinos y obtienes",
-   "Limpias la chimenea y encuentras",
-   "Desarrollas juegos para ganarte la vida y ganas",
-   "Trabajaste en la oficina horas extras por",
-   "Trabajas como secuestrador de novias y ganas",
-   "Alguien vino y representó una obra de teatro. Por mirar te dieron",
-   "Compraste y vendiste artículos y ganaste",
-   "Trabajas en el restaurante de la abuela como cocinera y ganas",
-   "Trabajas 10 minutos en un Pizza Hut local. Ganaste",
-   "Trabajas como escritor(a) de galletas de la fortuna y ganas",
-   "Revisas tu bolso y decides vender algunos artículos inútiles que no necesitas. Resulta que toda esa basura valía",
-   "Desarrollas juegos para ganarte la vida y ganas",
-   "Trabajas todo el día en la empresa por",
-   "Diseñaste un logo para una empresa por",
-   "¡Trabajó lo mejor que pudo en una imprenta que estaba contratando y ganó su bien merecido!",
-   "Trabajas como podador de arbustos y ganas",
-   "Trabajas como actor de voz para Bob Esponja y te las arreglaste para ganar",
-   "Estabas cultivando y Ganaste",
-   "Trabajas como constructor de castillos de arena y ganas",
-   "Trabajas como artista callejera y ganas",
-   "¡Hiciste trabajo social por una buena causa! por tu buena causa Recibiste",
-   "Reparaste un tanque T-60 averiado en Afganistán. La tripulación te pagó",
-   "Trabajas como ecologista de anguilas y ganas",
-   "Trabajas en Disneyland como un panda disfrazado y ganas",
-   "Reparas las máquinas recreativas y recibes",
-   "Hiciste algunos trabajos ocasionales en la ciudad y ganaste",
-   "Limpias un poco de moho tóxico de la ventilación y ganas",
-   "Resolviste el misterio del brote de cólera y el gobierno te recompensó con una suma de",
-   "Trabajas como zoólogo y ganas",
-   "Vendiste sándwiches de pescado y obtuviste",
-   "Reparas las máquinas recreativas y recibes",
-] 
+
