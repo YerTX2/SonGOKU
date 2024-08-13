2c6ede2def9835b003bc18581dd5349a0cb0af31
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-13 08:42:57 -0500

    Actualizar rpg-dulces.js

--- plugins/rpg-dulces.js
+++ plugins/rpg-dulces.js
@@ -3,10 +3,10 @@ let handler = async (m, {conn, usedPrefix}) => {
    if (who == conn.user.jid) return error 
    if (!(who in global.db.data.users)) return conn.reply(m.chat, 'El usuario no se encuentra en mi base de Datos.', m)
    let user = global.db.data.users[who]
-   await m.reply(`${who == m.sender ? `Tienes *${user.limit} 🍬 Dulces* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.limit} 🍬 Dulces* en su Cartera`}. `, null, { mentions: [who] })
+   await m.reply(`${who == m.sender ? `Tienes *${user.limit} ⚡ KI* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.limit} ⚡ KI* en su Cartera`}. `, null, { mentions: [who] })
 }
 
-handler.help = ['dulces']
+handler.help = ['ki'KI',']
 handler.tags = ['rpg']
 handler.command = ['wallet', 'cartera', 'dulces', 'bal', 'coins']
 handler.register = true 
