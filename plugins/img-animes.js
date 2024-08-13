7b21abdff109cb0d22652b76908ec53e9e58b0e2
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-13 12:47:04 -0500

    Actualizar img-animes.js

--- plugins/img-animes.js
+++ plugins/img-animes.js
@@ -10,5 +10,13 @@ conn.sendFile(m.chat, haha, 'error.jpg', `*${command}*`, m, null, rcanal)
 
 handler.command = handler.help = ['alisa', 'aihoshino', 'remcham', 'akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumitokisaki', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura']
 handler.tags = ['anime']
-
+} catch {
+}}
+handler.help = ['anime']
+handler.tags = ['img']
+handler.command = ['anime']
+handler.register = true 
+//handler.limit = 1
 export default handler
+
+
