0d162517565a7b50abb6721c1d20762c90ea40c7
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-13 02:30:35 -0500

    Actualizar dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, command, args, text, usedPrefix }) => {
         return conn.reply(m.chat, '*𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚎𝚕 𝚗𝚘𝚖𝚋𝚛𝚎 𝚍𝚎 𝚕𝚘 𝚚𝚞𝚎 𝚚𝚞𝚒𝚎𝚛𝚎𝚜 𝚋𝚞𝚜𝚌𝚊𝚛*', m);
     }
 
-    await m.react('🕓');
+    await m.react('⏳');
     let res = await yts(text);
     let play = res.videos[0];
 
@@ -16,21 +16,21 @@ let handler = async (m, { conn, command, args, text, usedPrefix }) => {
     let { title, thumbnail, ago, timestamp, views, videoId, url } = play;
 
     let txt = '```𝚈𝚘𝚞𝚃𝚞𝚋𝚎 𝙳𝚎𝚜𝚌𝚊𝚛𝚐𝚊𝚜```\n';
-    txt += '===========================\n';
+    txt += '╭━─━─━─━─≪✠≫─━─━─━─━╮\n';
     txt += `> *𝚃𝚒𝚝𝚞𝚕𝚘* : _${title}_\n`;
     txt += `> *𝙲𝚛𝚎𝚊𝚍𝚘* : _${ago}_\n`;
     txt += `> *𝙳𝚞𝚛𝚊𝚌𝚒𝚘𝚗* : _${timestamp}_\n`;
     txt += `> *𝚅𝚒𝚜𝚒𝚝𝚊𝚜* : _${views.toLocaleString()}_\n`;
     txt += `> *𝙻𝚒𝚗𝚔* : _https://www.youtube.com/watch?v=${videoId}_\n`;
-    txt += '===========================\n';
-    txt += '*ZxYerSixZx*';
+    txt += '┗─══──━══─| ✠ |─══━─═──┛ \n';
+    txt += '*🌀░S░o░n░G░o░k░u🌀*';
 
     await conn.sendButton2(m.chat, txt, '. ', thumbnail, [
         ['Audio', `${usedPrefix}ytmp3 ${url}`],
         ['Video', `${usedPrefix}ytmp4 ${url}`]
     ], null, [['Canal', 'https://whatsapp.com/channel/0029Vaj67qQJUM2Wa5Ey3y1v']], m);
 
-    await m.react('✅');
+    await m.react('🐉');
 };
 
 handler.help = ['play', 'play2', 'ytmp3'];
