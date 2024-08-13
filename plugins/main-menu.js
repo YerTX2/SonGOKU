8a3bbcd15cf7cf7ce4b76aea1fd791b68a0fc8c9
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-13 16:19:31 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -150,7 +150,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     
    
-let gifPlayback= await (await fetch(`https://telegra.ph/file/51b345301e8c7dc23f88d.mp4`)).buffer()
+let gif = await (await fetch(`https://telegra.ph/file/51b345301e8c7dc23f88d.mp4`)).buffer()
     await m.react('🐉')
 
  // await conn.sendMessage(m.chat, { video: { url:https://telegra.ph/file/51b345301e8c7dc23f88d.mp4 .getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
