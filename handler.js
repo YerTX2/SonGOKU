3c9c9ca99a25d74d9357cd1b518288c2b91adbc5
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-13 10:05:08 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -293,7 +293,7 @@ export async function handler(chatUpdate) {
                 else
                     m.exp += xp
                 if (!isPrems && plugin.limit && global.db.data.users[m.sender].limit < plugin.limit * 1) {
-                    conn.reply(m.chat, `Se agotaron tus *🍬 Dulces*`, m, rcanal)
+                    conn.reply(m.chat, `Ya no tiene más*⚡ Ki*`, m, rcanal)
                     continue
                 }
                 let extra = {
