a4591de16b892ef1d116ceda7a4d71062bea744b
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-15 17:35:44 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -149,7 +149,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     
-   
+et id = text ? text : m.chat    
 let pp = 'https://telegra.ph/file/f971feff8db773b6b3633.mp4'
 await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback:
 
