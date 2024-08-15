f0fc5c403be101fc056ac38cc28bd0b574463c99
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-14 21:47:02 -0500

    🐉ZxYerSixZx🐉

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,24 +1,27 @@
-import Scraper from "@SumiFX/Scraper"
+import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply('🌀 Ingresa el enlace del archivo de Mediafire junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.mediafire.com/file/433hbpsc95unywu/Oshi_no_Ko_01.mp4/file?dkey=jpivv6z5osa&r=1587`)
-if (!args[0].match(/mediafire/gi)) return m.reply('El enlace deve ser de un archivo de Mediafire.')
+if (!args[0]) return conn.reply(m.chat, '🚩 Ingrese el enlace de un archivo de Mediafire.', m, rcanal)
+if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, 'El enlace deve ser de un archivo de Mediafire.', m, rcanal)
+await m.react('🕓')
 try {
-let { title, ext, aploud, size, dl_url } = await Scraper.mediafire(args[0])
-if (size.includes('GB') || size.replace(' MB', '') > 4000) { return await m.reply('El archivo pesa mas de 4000 MB, se canceló la Descarga.')}
-let txt = `╭─⬣「 *Mediafire Download* 」⬣\n`
-    txt += `│  ≡◦ *🌀 Nombre ∙* ${title}\n`
-    txt += `│  ≡◦ *🪴 Subido ∙* ${aploud}\n`
-    txt += `│  ≡◦ *📚 MimeType ∙* ${ext}\n`
-    txt += `│  ≡◦ *⚖ Peso ∙* ${size}\n`
-    txt += `╰─⬣`
-await m.reply(txt)
+let { title, ext, aploud, size, dl_url } = await Starlights.mediafire(args[0])
+let txt = `乂  *M E D I A F I R E  -  D O W N L O A D*\n\n`
+    txt += `	⚔️  *Nombre* : ${title}\n`
+    txt += `	⚔️  *Peso* : ${size}\n`
+    txt += `	⚔️  *Publicado* : ${aploud}\n`
+    txt += `	⚔️  *MimeType* : ${ext}\n\n`
+    txt += `*- ↻ El archivo se esta enviando espere ATT HADES BOT . . .*`
+let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await conn.sendFile(m.chat, dl_url, title, null, m, null, { mimetype: ext, asDocument: true })
+await m.react('✅')
 } catch {
+await m.react('✖️')
 }}
-handler.help = ['mediafire <url mf>']
-handler.tags = ['downloader']
+handler.help = ['mediafire'].map(v => v + ' *<url>*')
+handler.tags = ['downloader', 'premium']
 handler.command = ['mediafire', 'mdfire', 'mf']
-handler.register = true 
-handler.limit = 500
+handler.premium = true 
+
 export default handler
