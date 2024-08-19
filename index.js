c01683603a4ec638b12faaf20c5191fa9faf4d36
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-18 21:07:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const rl = createInterface(process.stdin, process.stdout)
 const app = express()
 const port = process.env.PORT || 8080;
 
-say('Son\nGoku\n\nCreador y editor del bot Yer', {
+say('Son\nGoku', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
