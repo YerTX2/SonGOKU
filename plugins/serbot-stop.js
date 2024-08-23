c6be57a0ffa5814e35685042dffe7d78c807fbc7
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-23 00:24:49 -0500

    ZxYerSixZx

--- plugins/serbot-stop.js
+++ plugins/serbot-stop.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   if (global.conn.user.jid === conn.user.jid) {
   } else {
-    await conn.reply(m.chat, `Adiós Bot :(`, m)
+    await conn.reply(m.chat, `Adiós Ai :(`, m, rcanal)
     conn.ws.close()
   }
 }
