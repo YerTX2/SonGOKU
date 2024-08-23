3c33499b6592a1c2dd6b59b35ec99d801fee1153
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-22 23:25:57 -0500

    Actualizar group-tag.js

--- plugins/group-tag.js
+++ plugins/group-tag.js
@@ -5,7 +5,7 @@ conn.sendMessage(m.chat, { forward: m.quoted.fakeObj, mentions: users } )
 }
 handler.help = ['tag']
 handler.tags = ['group']
-handler.command = /^(n|tag)$/i
+handler.command = /^(tag)$/i
 handler.admin = true
 handler.group = true
 
