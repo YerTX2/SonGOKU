44323fd8670d9573a3426ae2400f3e2d95f66c70
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-27 23:05:53 -0500

    Actualizar downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -11,7 +11,7 @@ let txt = `乂  *M E D I A F I R E  -  D O W N L O A D*\n\n`
     txt += `	✩  *Peso* : ${size}\n`
     txt += `	✩  *Publicado* : ${aploud}\n`
     txt += `	✩  *MimeType* : ${ext}\n\n`
-    txt += `*- ↻ El archivo se esta enviando espera un momento, soy lenta. . .*`
+    txt += `*- ↻ Se está descargado su pedido 📥 espere un momento @SonGoku*`
 let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await conn.sendFile(m.chat, dl_url, title, null, m, null, { mimetype: ext, asDocument: true })
