9c5747afb3f79ab06cc204393e112628c36fbf76
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-08-28 10:46:51 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -18,6 +18,14 @@ import moment from 'moment-timezone'
     }
     let muptime = clockString(_muptime)
 
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, 'd ', h, 'h ', m, 'm ', s, 's '].map(v => v.toString().padStart(2, 0)).join('')
+}
+
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.owner = [
