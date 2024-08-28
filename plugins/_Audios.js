8f3cc8c4eb6dd9da38017bafd9e640691ee57764
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-08-28 10:35:46 -0300

    Actualizar plugins

---
+++ plugins/_Audios.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+let handler = async (m, { conn }) => {
+  let text = m.text.toLowerCase(); 
+  let user = global.db.data.users[m.sender];
+
+  const audioMap = {
+    'hola': './audios/hola.mp3',
+    'cómo estás': './audios/como_estas.mp3',
+    'goku': './audios/goku.mp3'
+  };
+
+  for (let key in audioMap) {
+    if (text.includes(key)) {
+      try {
+        if (fs.existsSync(audioMap[key])) {
+          await conn.sendFile(m.chat, audioMap[key], `${key}.mp3`, '', m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument });
+        } else {
+          m.reply('El audio correspondiente no se encontró.');
+        }
+      } catch (err) {
+        console.error(err);
+        m.reply('Ocurrió un error al intentar enviar el audio.');
+      }
+      break;
+    }
+  }
+};
+
+handler.customPrefix = /^(hola|cómo estás|goku)/i;
+handler.command = new RegExp;
+export default handler;
