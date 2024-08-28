81818ebdc10f10e91d1de7f14f1e7d0809ebed87
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-08-28 11:37:19 -0300

    Actualizar img-neko.js

--- plugins/img-neko.js
+++ plugins/img-neko.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, command }) => {
 let ne = await (await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')).text()
 let nek = ne.split('\n')
 let neko = pickRandom(nek)
-//conn.sendFile(m.chat, neko, 'error.jpg', `Uwu 🥺`, m)
-conn.sendButton(m.chat, 'Uwu 🥺', wm, neko, [['SIGUIENTE IMAGEN', `/${command}`]], null, null, m)
+conn.sendFile(m.chat, neko, 'error.jpg', `Uwu 🥺`, m)
+//conn.sendButton(m.chat, 'Uwu 🥺', wm, neko, [['SIGUIENTE IMAGEN', `/${command}`]], null, null, m)
 }
 handler.command = /^(neko)$/i
 handler.tags = ['img']
