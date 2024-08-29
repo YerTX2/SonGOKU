a97426316625a5348dc7e37a29ae8ad994b4fac8
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-29 05:09:31 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -150,7 +150,8 @@ let urls = [
 "https://telegra.ph/file/4f8676c687995ec8333cb.mp4",
 "https://telegra.ph/file/c4c2f5d0931ac30accca2.mp4", 
 "https://telegra.ph/file/0a9683f4aedcae4eca935.mp4", 
-"https://telegra.ph/file/3ba419b5970c3812dbac6.mp4" 
+"https://telegra.ph/file/3ba419b5970c3812dbac6.mp4", 
+"https://telegra.ph/file/c023e62cf15c5cb85caab.mp4" 
 ];
 let gifUrl = urls[Math.floor(Math.random() * urls.length)];
 await conn.sendMessage(m.chat, {video: {url: gifUrl}, gifPlayback: true, caption: text.trim(), mentions: [m.sender]}, {quoted: m});
