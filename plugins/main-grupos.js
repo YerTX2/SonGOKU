f11e791f3063afa2d5bf5666202ff9c6131256c0
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-08-30 15:00:14 -0500

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -4,7 +4,8 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://telegra.ph/file/0dbf7b27644e8dfa8baa4.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `*Hola!, te invito a formar parte de la comunidad de ░G░o░k░u░-░B░l░a░c░k░*
+let txt = `*Hola!, te invito a formar parte de la comunidad de 
+> SON GOKU / TEAM
 
 >  Enlace Grupo 🌀SonGoku🌀
 
@@ -23,7 +24,7 @@ Canal :
 
 > TEAM MoonLight Team
 > CANAL 'https://whatsapp.com/channel/0029Vablj8M05MUnustq3q1S' 
->GRUPO https://chat.whatsapp.com/GpbF3JCTEr2CSj3zLJ1IQj
+>GRUPO 'https://chat.whatsapp.com/GpbF3JCTEr2CSj3zLJ1IQj' 
 
 > 🚩 ${textbot}`
 await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
