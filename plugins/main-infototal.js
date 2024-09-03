75950def596c902a7d07efbda76d5c81f59be8c0
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-09-03 13:50:39 -0300

    Actualizar main-infototal.js

--- plugins/main-infototal.js
+++ plugins/main-infototal.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.reply(m.chat, `*✅ Total de Funciones* : ${totalf}`,m)
+conn.reply(m.chat, `*✅ Total de Funciones del bot* : ${totalf}`,m)
 }
 
 handler.help = ['totalfunciones']
