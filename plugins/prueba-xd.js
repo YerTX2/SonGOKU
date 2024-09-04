62c7812fa31cb13799eb315523dcb8df69d6546f
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-09-04 00:12:25 -0300

    Actualizar prueba-xd.js

--- plugins/prueba-xd.js
+++ plugins/prueba-xd.js
@@ -1,27 +1,27 @@
 import fetch from 'node-fetch';
 
-
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-async function getMail() {
-    try {
-        let response = await fetch('https://api.cafirexos.com/api/tempmail/getmail');
-        let text = await response.text();
-        
-        let correo = text.match(/"correo":\s*"([^"]+)"/)[1];
-        
-        await m.reply(correo);
-    } catch (error) {
-        await m.reply('Error fetching data:', error);
+    async function getMail() {
+        try {
+            let response = await fetch('https://api.cafirexos.com/api/tempmail/getmail');
+            let json = await response.json(); 
+
+            let correo = json.correo; 
+
+            await m.reply(correo);
+        } catch (error) {
+            await m.reply('Error fetching data: ' + error.message);
+        }
     }
-}
 
-getMail();
+    await getMail(); 
+};
 
-handler.help = ['getemail']
-handler.tags = ['search']
-handler.command = ['getemail']
-//handler.limit = 1
-handler.register = true
+handler.help = ['getemail'];
+handler.tags = ['search'];
+handler.command = ['getemail'];
+// handler.limit = 1;
+handler.register = true;
 
-export default handler
+export default handler;
