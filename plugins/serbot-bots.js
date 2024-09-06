aab8b0334912b8d517af5d57fddaa122c7bec495
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-06 12:07:30 -0500

    Actualizar serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -8,7 +8,7 @@ global.conns.forEach((conn) => {
 if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
 uniqueUsers.set(conn.user.jid.replace(/[^0-9]/g, ''), conn.user)}})
 
-const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  ✩  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  ✩  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  ✩  *Nombre* : ${user.name || 'Sub Bot ՏᴏɴᏀᴏᴋᴜ'}\n`
+const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  🇦🇱  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  🇦🇱  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  🇦🇱  *Nombre* : ${user.name || 'Sub Bot ՏᴏɴᏀᴏᴋᴜ'}\n`
   ).join('\n')
 
 const replyMessage = message.length === 0 ? "" : message
