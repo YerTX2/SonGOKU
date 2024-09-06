3476625ef65e3b7f050fd3aaa6cfe150edfaed9e
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-06 10:16:38 -0500

    Actualizar serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -1,25 +1,24 @@
 import ws from 'ws'
+import fetch from 'node-fetch'
 
-async function handler(m, { conn: stars, usedPrefix }) {
-  let uniqueUsers = new Map()
+async function handler(m, { conn: _envio, usedPrefix }) {
+const uniqueUsers = new Map()
 
-  global.conns.forEach((conn) => {
-    if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
-      uniqueUsers.set(conn.user.jid, conn)
-    }
-  })
+global.conns.forEach((conn) => {
+if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+uniqueUsers.set(conn.user.jid.replace(/[^0-9]/g, ''), conn.user)}})
 
-  let users = [...uniqueUsers.values()]
+const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  ✩  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  ✩  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  ✩  *Nombre* : ${user.name || 'Ai Hoshino'}\n`
+  ).join('\n')
 
-  let message = users.map((v, index) => `*${index + 1}.-* @${v.user.jid.replace(/[^0-9]/g, '')}\n*Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n*Nombre:* ${v.user.name || '-'}`).join('\n\n')
-  
-  let replyMessage = message.length === 0 ? '' : message
-  let totalUsers = users.length
-  let responseMessage = `*Total de Bots* : ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim()
+const replyMessage = message.length === 0 ? "" : message
+const totalUsers = uniqueUsers.size;
+const responseMessage = `${` –  *S E R B O T  -  S U B B O T S*\n\n${replyMessage.trim()}`.trim()}`
 
-  await stars.sendMessage(m.chat, { text: responseMessage, mentions: stars.parseMention(responseMessage) }, { quoted: m })
+let img = await (await fetch(`https://i.ibb.co/ZWtV5Ph/20240705-085716.jpg`)).buffer()
+
+await _envio.sendFile(m.chat, img, 'thumbnail.jpg', responseMessage, m, false, { mentions: _envio.parseMention(responseMessage) })
 }
-
 handler.command = ['listjadibot', 'bots']
 handler.help = ['bots']
 handler.tags = ['serbot']
