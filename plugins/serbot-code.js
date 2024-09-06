4c46bc92a5f4f6b0be877f1c6be2e32a7b9f5992
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-06 10:51:00 -0500

    Actualizar serbot-code.js

--- plugins/serbot-code.js
+++ plugins/serbot-code.js
@@ -53,7 +53,7 @@ const connectionOptions = {
   logger: pino({ level: 'silent' }),
   printQRInTerminal: false,
   mobile: MethodMobile, 
-  browser: ['孫ՏᴏɴᏀᴏᴋᴜ孫Sub-Bot', 'Edge', '2.0.0'],
+  browser: [ "Ubuntu", "Chrome", "20.0.04" ],
   auth: {
   creds: state.creds,
   keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
