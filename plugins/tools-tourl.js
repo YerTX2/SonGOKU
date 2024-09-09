f3a394fa392723cf49864e73918734151373244b
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-09 12:57:25 -0500

    Actualizar tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -5,28 +5,35 @@ import fetch from 'node-fetch'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, '🌀 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
-  await m.react('🕓')
+  if (!mime) return conn.reply(m.chat, '🚩 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
+  await m.react(rwait)
   try {
+  conn.reply(m.chat, '🚩 Convirtiendo la imagen en url...', m, {
+  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+  title: packname,
+  body: dev,
+  previewType: 0, thumbnail: icons,
+  sourceUrl: channel }}})
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   let img = await (await fetch(`${link}`)).buffer()
-  let txt = '`-  T E L E G R A P H  -  U P L O A D E R`\n\n'
-      txt += `  *🔖 Enlace* : ${link}\n`
-      txt += `  *🔖 Acortado* : ${await shortUrl(link)}\n`
-      txt += `  *🔖 Tamaño* : ${formatBytes(media.length)}\n`
-      txt += `  *🔖 Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
-      txt += `🌀 *${textbot}*`
+  let txt = `乂  *L I N K - E N L A C E*  乂\n\n`
+      txt += `*» Enlace* : ${link}\n`
+      txt += `*» Acortado* : ${await shortUrl(link)}\n`
+      txt += `*» Tamaño* : ${formatBytes(media.length)}\n`
+      txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
+      txt += `> *${dev}*`
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await m.react('✅')
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fkontak, rcanal)
+await m.react(done)
 } catch {
-await m.react('✖️')
+await conn.reply(m.chat, '🌱 Ocurrió un error', m, fake)
+await m.react(error)
 }}
 handler.help = ['tourl']
-handler.tags = ['tools']
-handler.command = /^(tourl|upload)$/i
+handler.tags = ['transformador']
+handler.command = ['tourl', 'upload']
 export default handler
 
 function formatBytes(bytes) {
@@ -39,6 +46,6 @@ function formatBytes(bytes) {
 }
 
 async function shortUrl(url) {
-	let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-	return await res.text()
+        let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+        return await res.text()
 }
