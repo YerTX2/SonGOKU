6e8ebe7158752cab5986effbb9b12194c030b7d4
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-10 00:50:35 -0500

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -3,13 +3,13 @@ import path from 'path';
 
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: '⚠️ Este comando solo puede ser utilizado en el bot principal.'}, {quoted: m});
+    return conn.sendMessage(m.chat, {text: '⚠️ Este comando solo puede ser utilizado en el bot principal de 孫ՏᴏɴᏀᴏᴋᴜ孫.'}, {quoted: m});
   }
   await conn.sendMessage(m.chat, {text: '✅ Iniciando...'}, {quoted: m});
   const sessionPath = './sessions'
   try {
     if (!existsSync(sessionPath)) {
-      return await conn.sendMessage(m.chat, {text: '⚠️ La carpeta sessions no existe o está vacía.'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '⚠️ La carpeta session de 孫ՏᴏɴᏀᴏᴋᴜ孫 no existe o está vacía.'}, {quoted: m});
     }
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
@@ -20,14 +20,14 @@ const handler = async (m, { conn, usedPrefix }) => {
       }
     }
     if (filesDeleted === 0) {
-      await conn.sendMessage(m.chat, {text: '⚠️ No se encontró ningún archivo para eliminar en la carpeta sessions.'}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: '⚠️ No se encontró ningún archivo para de ՏᴏɴᏀᴏᴋᴜ eliminar en la carpeta sessions.'}, {quoted: m});
     } else {
-      await conn.sendMessage(m.chat, {text: `✅ Se eliminaron ${filesDeleted} archivos.`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `✅ Se eliminaron ${filesDeleted} archivos de ՏᴏɴᏀᴏᴋᴜ.`}, {quoted: m});
     }
   } catch {
-    await conn.sendMessage(m.chat, {text: '⚠️ Ocurrió un error al eliminar los archivos de sesión.'}, {quoted: m});
+    await conn.sendMessage(m.chat, {text: '⚠️ Ocurrió un error al eliminar los archivos de sesión de ՏᴏɴᏀᴏᴋᴜ.'}, {quoted: m});
   }
-  await conn.sendMessage(m.chat, {text: `¡Hola! ¿Ahora me ves?`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `¡Hola! ¿Ahora me ves Att ՏᴏɴᏀᴏᴋᴜ?`}, {quoted: m});
 };
 handler.tags = ['owner']
 handler.help = ['dsowner', 'ds']
