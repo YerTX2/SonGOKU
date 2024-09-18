4e1b5d48f8595e6d0c5f722910a57aee21f2cc4a
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-18 11:16:52 -0500

    Actualizar youtube-ytsearch.js

--- plugins/youtube-ytsearch.js
+++ plugins/youtube-ytsearch.js
@@ -1,25 +1,74 @@
-import Scraper from "yt-search"
+import Scraper from "@SumiFX/Scraper"
+const handler = async (m, { conn, usedPrefix, args, command }) => {
+try {
+const text = args.length >= 1 ? args.slice(0).join(" ") : (m.quoted && m.quoted?.text || m.quoted?.caption || m.quoted?.description) || null
 
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-    if (!text) return conn.reply(m.chat, '🔍 Ingresa el título de un video o canción de YouTube.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m)
-    let results = await Scraper.ytsearch(text)
-    if (!results || !results.length) return conn.reply(m.chat, `No se encontraron resultados.`, m)
-    let img = results[0].thumbnail
-    let txt = `╭─⬣「 *YouTube Search* 」⬣\n`
-    results.forEach((video, index) => {
-        txt += ` │  ≡◦ *🔍 Nro ∙* ${index + 1}\n`
-        txt += ` │  ≡◦ *⚔️ Titulo ∙* ${video.title}\n`
-        txt += ` │  ≡◦ *🕜 Duración ∙* ${video.duration}\n`
-        txt += ` │  ≡◦ *🪴 Publicado ∙* ${video.published}\n`
-        txt += ` │  ≡◦ *📚 Autor ∙* ${video.author}\n`
-        txt += ` │  ≡◦ *⛓ Url ∙* ${video.url}\n`
-        txt += ` ╰──────────⬣`
-        txt += `\n`
-    })
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
+if (!text) return conn.reply(m.chat, `🤍 *Escriba el título de algún vídeo de Youtube*\n\nEjemplo, ${usedPrefix + command} SonGoku edits`, m, rcanal, )
+
+const { all: [bestItem, ...moreItems] } = await ytSearch(text)
+const videoItems = moreItems.filter(item => item.type === 'video')
+const formattedData = {
+title: `\`[ YOUTUBE - SEARCH ]\`\n\n> 🤍 *\`Titulo :\`* ${bestItem.title}\n> 🤍 *\`Duración :\`* ${bestItem.timestamp}\n> 🤍 *\`Visitas :\`* ${bestItem.views}\n> 🤍 *\`Subido :\`* ${bestItem.ago}\n> 🤍 *\`Url :\`* ${bestItem.url}`,
+rows: [{
+title: "Vídeo más Popular 😼",
+highlight_label: "Popular",
+rows: [{
+header: bestItem.title,
+id: `${usedPrefix}ytmp4 ${bestItem.url}`,
+title: bestItem.title,
+description: ""
+}]
+}, {
+title: "Videos Encontrados 🤍",
+rows: videoItems.map(({
+title,
+url,
+description
+}, index) => ({
+header: `${index + 1}). ${title}`,
+id: `.ytmp4 ${url}`,
+title: title,
+description: ""
+}))
+}]
 }
-handler.help = ['ytsearch <búsqueda>']
-handler.tags = ['search']
-handler.command = ['ytsearch', 'yts']
-handler.register = true 
+const emojiMap = {
+type: "🎥",
+videoId: "🆔",
+url: "🔗",
+title: "📺",
+description: "📝",
+image: "🖼️",
+thumbnail: "🖼️",
+seconds: "⏱️",
+timestamp: "⏰",
+ago: "⌚",
+views: "👀",
+author: "👤"
+}
+
+const caption = Object.entries(bestItem).map(([key, value]) => {
+const formattedKey = key.charAt(0).toUpperCase() + key.slice(1)
+const valueToDisplay = key === 'views' ? new Intl.NumberFormat('en', { notation: 'compact' }).format(value) : key === 'author' ? `Nombre: ${value.name || 'Desconocido'}\nURL: ${value.url || 'Desconocido'}` : value || 'Desconocido';
+return ` ${emojiMap[key] || '🔹'} *${formattedKey}:* ${valueToDisplay}`}).join('\n')
+
+await conn.sendButtonMessages(m.chat, [
+[formattedData.title, titulowm2, bestItem.image || logo, [
+], null, [
+[]
+],
+[["ʀᴇꜱᴜʟᴛᴀᴅᴏꜱ 🍂", formattedData.rows]]
+]], m, fake)
+
+} catch (error) {
+console.error(error)
+conn.reply(m.chat, `Ocurrió un error.${error}`, m)
+}
+}
+
+handler.help = ['ytsearch *<text>*']
+handler.tags = ['dl']
+handler.command = /^playlist|ytbuscar|yts(earch)?$/i
+handler.register = true
+handler.estrellas = 2
 export default handler
