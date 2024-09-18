67733e829ede68c8ddbbf8e92ce13368323e8419
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-18 11:12:21 -0500

    Actualizar youtube-ytsearch.js

--- plugins/youtube-ytsearch.js
+++ plugins/youtube-ytsearch.js
@@ -1,4 +1,4 @@
-import Scraper from "@SumiFX/Scraper"
+import Scraper from "yt-search"
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
     if (!text) return conn.reply(m.chat, '🔍 Ingresa el título de un video o canción de YouTube.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m)
