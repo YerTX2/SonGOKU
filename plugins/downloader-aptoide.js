1b121ab6af3ae868ea77bace7271548573ed2c47
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-19 15:42:04 -0500

    Actualizar downloader-aptoide.js

--- plugins/downloader-aptoide.js
+++ plugins/downloader-aptoide.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Debes proporcionar el nombre de una aplicación para buscar.\nEjemplo:\n${usedPrefix + command} WhatsApp`;
 
     try {
-        await m.reply(`🐉孫ՏᴏɴᏀᴏᴋᴜ孫🐉 Buscando la aplicación "${text}"...`);
+        await m.reply(`🔍 Buscando la aplicación "${text}"...`);
 
         let res = await fetch(`https://api.diego-ofc.site/v2/apk-dl?text=${encodeURIComponent(text)}`);
         if (!res.ok) throw `🚩 Error en la respuesta de la API: ${res.status}`;
@@ -21,21 +21,21 @@ let nombre = json.name;
         caption += `*Enlace:* ${link}\n`;
        caption+=  `*icono:*  ${icono2}\n`
         caption += `*Lasupdate:* ${lastupdate2}\n`
-        caption += `*Descargando APK 🐉孫ՏᴏɴᏀᴏᴋᴜ孫🐉.*`;
+        caption += `*Descargando APK...*`;
 
         await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: caption }, { quoted: m });
-        
+
         // No necesitas volver a hacer un fetch en el link, ya que es un enlace directo
         await conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'application/vnd.android.package-archive', fileName: `${nombre}.apk`, caption: null }, { quoted: m });
 
     } catch (e) {
         console.error(e);
-        throw `🐉 Hubo un error al buscar o descargar la aplicación "${text}": ${e.message || e}`;
+        throw `🍟 Hubo un error al buscar o descargar la aplicación "${text}": ${e.message || e}`;
     }
 }
 
 handler.help = ['apk'].map(v => v + ' <nombre de la aplicación>');
- handler.tags = ['downloader'] 
+handler.tags = ['search', 'dl'];
 handler.command = /^(apk|apkd|apkdownload)$/i;
 
 export default handler;
