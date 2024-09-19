6ec9027c9a53eab0e984811585676dc58dae9f09
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-19 05:50:55 -0500

    Actualizar playGoku.js

--- plugins/playGoku.js
+++ plugins/playGoku.js
@@ -179,7 +179,7 @@ const handler = async (m, { command, usedPrefix, conn, text }) => {
   }
 };
 
-handler.command = ['play.1','play.2','playyt']
+handler.command = ['play.1','play.2','play']
 handler.register = true;
 handler.group = true;
 export default handler;
