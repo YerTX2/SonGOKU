9d64748ab28eea0b8d5263c3b2307063cea6e8bb
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-09-20 06:23:17 -0500

    Actualizar plugins

---
+++ plugins/ytmp3doc goku.js
@@ -0,0 +1,31 @@
+import Starlights from '@StarlightsTeam/Scraper'
+import fetch from 'node-fetch' 
+let limit = 200
+
+let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, command }) => {
+if (!args[0].match(/youtu/gi)) return star.reply(m.chat, '🚩 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
+
+await m.react('🕓')
+try {
+let { title, size, quality, thumbnail, dl_url } = await Starlights.ytmp3(args[0])
+
+let img = await (await fetch(`${thumbnail}`)).buffer()
+if (size.split('MB')[0] >= limit) return star.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal).then(_ => m.react('✖️'))
+        let txt = '`乂  Y O U T U B E  -  M P 3 D O C`\n\n'
+       txt += `        ✩   *Titulo* : ${title}\n`
+       txt += `        ✩   *Calidad* : ${quality}\n`
+       txt += `        ✩   *Tamaño* : ${size}\n\n`
+       txt += `> *- ↻ El audio se esta enviando espera un momento, soy lenta. . .*`
+await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+await star.sendMessage(m.chat, { document: { url: dl_url }, caption: '', mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m })
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['ytmp3doc *<link yt>*']
+handler.tags = ['downloader']
+handler.command = ['ytmp3doc', 'ytadoc'] 
+//handler.limit = 1
+handler.register = true 
+
+export default handler
