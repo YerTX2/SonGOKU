2d987e03d7c2fecf0f0a21c5079aece62e1e61b9
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-03 03:37:34 -0500

    Actualizar youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -1,22 +1,51 @@
-import Scraper from "@SumiFX/Scraper"
+import fg from 'api-dylux'
+import yts from 'yt-search'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+let limit = 100
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply('🔍 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`)
-if (!args[0].match(/youtu/gi)) return conn.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m)
+let handler = async (m, { conn, args, text, isPrems, isOwner, usedPrefix, command }) => {
+if (!args || !args[0]) return conn.reply(m.chat, `🍟 Escribe la URL de un video de YouTube que deseas descargar.`, m)
+if (!args[0].match(/youtu/gi)) return conn.reply(m.chat,`Verifica que la *URL* sea de YouTube`, m).then(_ => m.react('✖️'))
+let q = args[1] || '360p'
 
-let user = global.db.data.users[m.sender]
+await m.react('🕓')
 try {
-let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp4(args[0])
-if (size.includes('GB') || size.replace(' MB', '') > 120) { return await m.reply('El archivo pesa mas de 120 MB, se canceló la Descarga Descargue en MP4DOC PARA QUE DE ENVÍE SU VIDEO.')}
-let txt = ` ╭─⬣「 *YouTube Descarga* 」⬣\n`
-    txt += `│: ̗̀➛ *🔍 Titulo ∙* ${title}\n`
-    txt += `│: ̗̀➛ *🪴 Calidad ∙* ${quality}\n`
-    txt += `│: ̗̀➛ *⚖ Peso ∙* ${size}\n`
-    txt += `╰─⬣`
-await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
-await conn.sendFile(m.chat, dl_url, title + '.mp4', `*🔍 Titulo ∙* ${title}\n*🪴 Calidad ∙* ${quality}`, m, false, { asDocument: user.useDocument })
+const yt = await fg.ytv(args[0], q)
+let { title, dl_url, size } = yt 
+let vid = (await yts(text)).all[0]
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
+
+await conn.sendMessage(m.chat, {
+        text: `🍭 *Título ∙* ${title}\n⚖️ *Tamaño ∙* ${size}\n\n*↻ Espera @${m.sender.split`@`[0]}, soy lenta. . .*`,
+        contextInfo: { 
+          mentionedJid: [m.sender],
+        }
+      }, { quoted: m })
+
+await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n00:15 ━━━━●────── ${vid.timestamp}`, m)
+await m.react('✅')
 } catch {
-}}
+try {
+let yt = await fg.ytmp4(args[0], q)
+let { title, size, dl_url } = yt
+let vid = (await yts(text)).all[0]
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
+
+await conn.sendMessage(m.chat, {
+        text: `🍭 *Título ∙* ${title}\n⚖️ *Tamaño ∙* ${size}\n\n*↻ Espera @${m.sender.split`@`[0]}, soy lenta. . .*`,
+        contextInfo: { 
+          mentionedJid: [m.sender],
+        }
+      }, { quoted: m })
+
+await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n00:15 ━━━━●────── ${vid.timestamp}`, m)
+await m.react('✅')
+} catch {
+await conn.reply(m.chat,`*☓ Ocurrió un error inesperado*`, m).then(_ => m.react('✖️'))
+//console.error(error)
+}}}
 handler.help = ['ytmp4 <yt url>']
 handler.tags = ['downloader']
 handler.command = ['ytmp4', 'yt', 'ytv']
