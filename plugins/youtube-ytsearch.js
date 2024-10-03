886a1c26af5fd9d01c003990e59e7bcd8019d7c4
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-03 03:05:08 -0500

    Actualizar youtube-ytsearch.js

--- plugins/youtube-ytsearch.js
+++ plugins/youtube-ytsearch.js
@@ -1,25 +1,85 @@
-import Scraper from "@SumiFX/Scraper"
+import { prepareWAMessageMedia, generateWAMessageFromContent, getDevice } from '@whiskeysockets/baileys'
+import yts from 'yt-search';
+import fs from 'fs';
 
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-    if (!text) return conn.reply(m.chat, '🔍 Ingresa el título de un video o canción de YouTube.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m)
-    let results = await Scraper.ytsearch(text)
-    if (!results || !results.length) return conn.reply(m.chat, `No se encontraron resultados.`, m)
-    let img = results[0].thumbnail
-    let txt = `╭─⬣「 *YouTube Search* 」⬣\n`
-    results.forEach((video, index) => {
-        txt += ` │  ≡◦ *🔍 Nro ∙* ${index + 1}\n`
-        txt += ` │  ≡◦ *⚔️ Titulo ∙* ${video.title}\n`
-        txt += ` │  ≡◦ *🕜 Duración ∙* ${video.duration}\n`
-        txt += ` │  ≡◦ *🪴 Publicado ∙* ${video.published}\n`
-        txt += ` │  ≡◦ *📚 Autor ∙* ${video.author}\n`
-        txt += ` │  ≡◦ *⛓ Url ∙* ${video.url}\n`
-        txt += ` ╰──────────⬣`
-        txt += `\n`
-    })
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
-}
-handler.help = ['ytsearch <búsqueda>']
-handler.tags = ['search']
-handler.command = ['ytsearch', 'yts']
-handler.register = true 
-export default handler
+const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
+    const datas = global;
+    const device = await getDevice(m.key.id);
+
+  if (!text) throw `⚠️ *Error*`;
+
+  if (device !== 'desktop' || device !== 'web') {      
+
+  const results = await yts(text);
+  const videos = results.videos.slice(0, 20);
+  const randomIndex = Math.floor(Math.random() * videos.length);
+  const randomVideo = videos[randomIndex];
+
+  var messa = await prepareWAMessageMedia({ image: {url: randomVideo.thumbnail}}, { upload: conn.waUploadToServer })
+  const interactiveMessage = {
+    body: { text: `*🍟 Resultados De:* ${text}\n*🍟 Resultados obtenidos:* ${results.videos.length}`.trim() },
+    footer: { text: `${global.wm}`.trim() },  
+      header: {
+          title: `*•/• YouTube - Search •/•*`,
+          hasMediaAttachment: true,
+          imageMessage: messa.imageMessage,
+      },
+    nativeFlowMessage: {
+      buttons: [
+        {
+          name: 'single_select',
+          buttonParamsJson: JSON.stringify({
+            title: 'SELECCIONE AQUÍ',
+            sections: videos.map((video) => ({
+              title: video.title,
+              rows: [
+                {
+                  header: video.title,
+                  title: video.author.name,
+                  description: 'Descargar MP3',
+                  id: `${prefijo}fgmp3 ${video.url}`
+                },
+                {
+                  header: video.title,
+                  title: video.author.name,
+                  description: 'Descargar MP4',
+                  id: `${prefijo}fgmp4 ${video.url}`
+                }
+              ]
+            }))
+          })
+        }
+      ],
+      messageParamsJson: ''
+    }
+  };        
+
+        let msg = generateWAMessageFromContent(m.chat, {
+            viewOnceMessage: {
+                message: {
+                    interactiveMessage,
+                },
+            },
+        }, { userJid: conn.user.jid, quoted: m })
+      conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id});
+
+  } else {
+  const results = await yts(text);
+  const tes = results.all;
+  const teks = results.all.map((v) => {
+    switch (v.type) {
+      case 'video': return `
+° *_${v.title}_*
+↳ 🫐 *_Url_* ${v.url}
+↳ 🕒 *_Fecha_* ${v.timestamp}
+↳ 📥 *_fecha_* ${v.ago}
+↳ 👁 *_Vista_* ${v.views}`;
+    }
+  }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
+  conn.sendFile(m.chat, tes[0].thumbnail, 'error.jpg', teks.trim(), m);      
+  }    
+};
+handler.help = ['ytsearch <texto>'];
+handler.tags = ['search'];
+handler.command = /^(playlist|yts|searchyt|yts|videosearch|audiosearch)$/i;
+export default handler;
