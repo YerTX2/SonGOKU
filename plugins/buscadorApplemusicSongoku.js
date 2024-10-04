a036524a913c3bd21b1cf5c208f7ab552ab4f12e
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-03 21:14:06 -0500

    Actualizar buscadorApplemusicSongoku.js

--- plugins/buscadorApplemusicSongoku.js
+++ plugins/buscadorApplemusicSongoku.js
@@ -1,25 +1,33 @@
-import yts from 'music.apple-search'
+let fetch = require("node-fetch");
 
-let handler = async (m, {conn, usedPrefix, text }) => {
-   if (!text) return conn.reply(m.chat, '*🚩 Ingresa lo que deseas buscar en YouTube.*', m)
-   await m.react('🕓')
-   let results = await yts(text)
-   let res = results.all.map(v => v).filter(v => v.type == "audio")
-   if (!res.length) return conn.reply(m.chat, 'No se encontraron resultados, intente con un nombre más Corto.', m).then(_ => m.react('✖️'))
-   let txt = `*Apple - Search*`
-   for (let i = 0; i < (30 <= res.length ? 30 : res.length); i++) {
-      txt += `\n\n`
-          txt += `        ◦  *Titulo* : ${res[i].title}\n`
-          txt += `        ◦  *Duración* : ${res[i].timestamp || '×'}\n`
-          txt += `        ◦  *Publicado* : ${res[i].ago}\n`
-          txt += `        ◦  *Autor* : ${res[i].author.name || '×'}\n`
-          txt += `        ◦  *Url* : ${'https://music.apple.com/us'\n`
-          }
-   await conn.sendFile(m.chat, res[0].image, '', txt, m)
-   await m.react('✅')
-}
-//handler.help = ['ytsearch']
-//handler.tags = ['search']
-//handler.command = ['applesearch', 'applemusicsearch']
-handler.register = true 
-export default handler
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text)
+    throw `
+*${usedPrefix}${command} <name animal>*
+example:
+*${usedPrefix}${command} <dog>*\n
+┌〔 Options 〕
+├ dog
+├ cat
+├ panda
+├ fox
+├ red_panda
+├ koala
+├ birb
+├ raccoon
+├ kangaroo
+└────
+`.trim();
+  let res = await fetch(
+    API("https://some-random-api.ml", "/animal/" + text, {})
+  );
+  if (!res.ok) throw `${res.status} ${res.statusText}`;
+  let json = await res.json();
+  if (json.image) await conn.sendFile(m.chat, json.image, "", `${json.fact}\n\n~kazukafu`, m);
+  else throw json;
+};
+handler.help = ["animal"].map((v) => v + " <animal>");
+handler.tags = ["internet"];
+handler.command = /^(animal|animalfact)$/i;
+
+module.exports = handler;
