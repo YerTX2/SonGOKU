60afe071d0be3346fce43445a6241193b56a5443
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-12 04:14:04 -0500

    Actualizar dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -25,10 +25,10 @@ let handler = async (m, { conn, command, args, text, usedPrefix }) => {
     txt += '·̇·̣̇̇·̣̣̇·̣̇̇·̇ 🇦🇱•🐉•୨୧┈┈┈୨୧•🐉•🇦🇱 ·̇·̣̇̇·̣̣̇·̣̇̇·̇ \n';
     
     await conn.sendButton2(m.chat, txt, '. ', thumbnail, [
-        ['MP3', `${usedPrefix}ytmp3 ${url}`],
-        ['MP3DOC', `${usedPrefix}ytmp3doc ${url}`],
-        ['MP4', `${usedPrefix}ytmp4 ${url}`], 
-        ['MP4DOC', `${usedPrefix}ytmp4doc ${url}`]
+        ['MP3🎵', `${usedPrefix}ytmp3 ${url}`],
+        ['MP3DOC🎵📄', `${usedPrefix}ytmp3doc ${url}`],
+        ['MP4📹', `${usedPrefix}ytmp4 ${url}`], 
+        ['MP4DOC📹📄', `${usedPrefix}ytmp4doc ${url}`]
         ], null, [['Canal', 'https://whatsapp.com/channel/0029Vaj67qQJUM2Wa5Ey3y1v']], m);
 
     await m.react('✅');
