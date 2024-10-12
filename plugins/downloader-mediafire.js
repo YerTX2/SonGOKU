3659e4cd3d2b3e5ac0a28f8a3310b954d6c36d11
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-12 04:39:54 -0500

    Actualizar downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,60 +1,67 @@
-import fetch from 'node-fetch'
-import { mediafiredl } from '@bochilteam/scraper'
-import fg from 'api-dylux'
-let free = 150 // limite de descarga
-let prem = 300 //si su servidor tienes menos de 2GB baja el límite
-let handler = async (m, { conn, args, text, usedPrefix, command, isOwner, isPrems }) => {
+import axios from 'axios';
+import path from 'path';
 
-   if (!args[0]) throw `✳️ Ingrese el link de mediafire junto al comando`
-    if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrecto`
-    m.react(rwait)
-
-    let limit = isPrems || isOwner ? prem : free
-        let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-    let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
-    try {
-    let res = await mediafiredl(args[0])
-    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let isLimit = limit * 1024 < filesize
-    let caption = `
-   ≡ *MEDIAFIRE*
-▢ *Nombre:* ${filename}
-▢ *Tamaño:* ${filesizeH}
-▢ *Extension:* ${ext}
-▢ *Subido:* ${aploud}
-${isLimit ? `\n▢ El archivo supera el límite de descarga *+${free} MB*\nPásate a premium para poder descargar archivos más de *${prem} MB*` : ''} 
-`.trim()
-    await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)  
-    if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-    m.react(done)
-
-    } catch {
-
-        try {
-        let res = await fg.mediafireDl(args[0])
-     let { url, url2, filename, ext, upload_date, filesize, filesizeB } = res
-    let isLimit = limit * 1024 < filesizeB
-    let caption = `
-   ≡ *MEDIAFIRE*
-▢ *Nombre:* ${filename}
-▢ *Tamaño:* ${filesize}
-▢ *Extension:* ${ext}
-▢ *Subido:* ${upload_date}
-${isLimit ? `\n▢ El archivo supera el límite de descarga *+${free} MB*\nPásate a premium para poder descargar archivos más de *${prem} MB*` : ''} 
-`.trim()
-
-await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
-if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-    m.react(done)
-} catch {
-    m.reply(`Error: intenta con otro link`)
-}
-
+let handler = async (message, { conn, args, usedPrefix, text, command }) => {
+  try {
+    if (!text) {
+      return message.reply(`${usedPrefix + command} https://www.mediafire.com/file/example`);
+    }
+    const response = await axios.get(`https://remcham-bot.vercel.app/api/mediafire?url=${text}`);
+    const { data } = response.data;
+    if (!response.data.success) {
+      return message.reply('Error: No se pudo obtener los detalles del archivo.');
+    }
+    const {
+      url,
+      filename,
+      filesizeH,
+      ext,
+      filetype,
+      uploadDate
+    } = data;
+    const downloadingMessage = `🌩️ *_ꜱᴜ ᴅᴇꜱᴄᴀʀɢᴀ ᴇꜱᴛᴀ ᴄᴏᴍᴇɴᴢᴀɴᴅᴏ...._*`;
+    const imageUrl = 'https://i.pinimg.com/736x/e4/7c/be/e47cbe22aae75ef2f22962cc022d6bac.jpg';
+    const responseImg = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    await conn.sendFile(message.chat, responseImg.data, 'thumbnail.png', downloadingMessage, message);
+    const caption = `*_ＤＥＳＣＡＲＧＡ ＥＸＩＴＯＳＡ..._*\n*_ᴀʀᴄʜɪᴠᴏ_*: ${filename}\n*_ᴛᴀᴍᴀÑᴏ_*: ${filesizeH}\n*_ᴛɪᴘᴏ_*: ${filetype}\n*_ᴇxᴛᴇɴᴄɪᴏɴ_*: ${ext}\n*_ᴘᴜʙʟɪᴄᴀᴅᴏ ᴇʟ_*: ${uploadDate}`;
+    const fileData = await axios({
+      method: 'get',
+      url,
+      responseType: 'arraybuffer'
+    });
+    const fileExtension = path.extname(filename).toLowerCase();
+    const mimeTypes = {
+      '.mp4': 'video/mp4',
+      '.pdf': 'application/pdf',
+      '.zip': 'application/zip',
+      '.rar': 'application/x-rar-compressed',
+      '.7z': 'application/x-7z-compressed',
+      '.jpg': 'image/jpeg',
+      '.jpeg': 'image/jpeg',
+      '.png': 'image/png',
+      '.apk': 'application/vnd.android.package-archive'
+    };
+    let mimetype = mimeTypes[fileExtension] || 'application/octet-stream';
+    await conn.sendFile(message.chat, fileData.data, filename, caption, message, null, { mimetype, asDocument: true });
+  } catch (error) {
+    return message.reply(`Error: ${error.message}`);
   }
+};
 
-}
 handler.help = ['mediafire'].map(v => v + ' *<url>*')
 handler.tags = ["Descargas de archivos"]
 handler.command = ['mediafire', 'mdfire', 'mf']
 handler.premium = true
 
+
+export default handler;
+
+function formatBytes(bytes) {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log (k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
