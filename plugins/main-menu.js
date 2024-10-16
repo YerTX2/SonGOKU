98abb5a1ba46f98ee72551230de4651eb4997622
ZxYerSixZx <oroscoyeferson@gmail.com>
2024-10-15 22:37:05 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -138,7 +138,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     
-let img = await (await fetch(`https://qu.ax/mtM.jpg`)).buffer()
+let img = await (await fetch(`https://i.ibb.co/6X35QcR/file.jpg`)).buffer()
     
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
