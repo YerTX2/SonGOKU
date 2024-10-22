d539cfc291a7c220e59ec36eca814fbb15a1492b
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-10-22 17:37:19 -0500

    Actualizar youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -1,22 +1,54 @@
-case 'play2': {
-const yts = require("youtube-yts");
-if (global.db.data.users[m.sender].registered < true) return reply(info.registra);
-if (!isGroupAdmins) return;
-if (!text) return conn.sendMessage(from, { text: `*🚩 ${lenguaje.sms.text}*\n${prefix + command} ozuna` }, { quoted: msg });
-m.react("⌛");
-const videoSearch = await yts(text);
-if (!videoSearch.all.length) {
-return m.react("❌");
-}
-const vid = videoSearch.all[0];
-const videoUrl = vid.url;
-const apiUrl = `https://deliriussapi-oficial.vercel.app/download/ytmp4?url=${encodeURIComponent(videoUrl)}`;
-const apiResponse = await fetch(apiUrl);
-const delius = await apiResponse.json();
+import fg from 'api-dylux'
+import yts from 'yt-search'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+let limit = 130
 
-if (!delius.status) {
-return m.react("❌")}
-const downloadUrl = delius.data.download.url;
-await conn.sendMessage(m.chat, { video: { url: downloadUrl }, caption: `↻ ◁ II ▷ ↺`}, {quoted: m})
-m.react("✅")}
-break
+let handler = async (m, { conn, args, text, isPrems, isOwner, usedPrefix, command }) => {
+if (!args || !args[0]) return conn.reply(m.chat, `🐉 Escribe la URL de un video de YouTube que deseas descargar.`, m)
+if (!args[0].match(/youtu/gi)) return conn.reply(m.chat,`Verifica que la *URL* sea de YouTube`, m).then(_ => m.react('✖️'))
+let q = args[1] || '480p'
+
+await m.react('🕓')
+try {
+const yt = await fg.ytv(args[0], q)
+let { title, dl_url, size } = yt 
+let vid = (await yts(text)).all[0]
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
+
+await conn.sendMessage(m.chat, {
+        text: `🇦🇱 *Título ∙* ${title}\n⚖️ *Tamaño ∙* ${size}\n\n*↻ Espera @${m.sender.split`@`[0]}, se está enviando su video en 480P. . .*`,
+        contextInfo: { 
+          mentionedJid: [m.sender],
+        }
+      }, { quoted: m })
+
+await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n Duración ${vid.timestamp}`, m)
+await m.react('✅')
+} catch {
+try {
+let yt = await fg.ytmp4(args[0], q)
+let { title, size, dl_url } = yt
+let vid = (await yts(text)).all[0]
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
+
+await conn.sendMessage(m.chat, {
+        text: `🇦🇱 *Título ∙* ${title}\n⚖️ *Tamaño ∙* ${size}\n\n*↻ Espera @${m.sender.split`@`[0]},se  está enviando su video . . .*`,
+        contextInfo: { 
+          mentionedJid: [m.sender],
+        }
+      }, { quoted: m })
+
+await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n Duración ${vid.timestamp}`, m)
+await m.react('✅')
+} catch {
+await conn.reply(m.chat,`*☓ Ocurrió un error inesperado pesa más de 130MB use el comando ytmp4doc para descargar *`, m).then(_ => m.react('✖️'))
+//console.error(error)
+}}}
+handler.help = ['ytmp4 <yt url>']
+handler.tags = ['downloader']
+handler.command = ['ytmp4', 'yt', 'ytv']
+handler.register = true 
+//handler.limit = 1
+export default handler
