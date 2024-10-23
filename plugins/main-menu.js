ece28f7dcb8015ff13594efb42db3edc0fe59755
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-10-23 13:40:16 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -2,72 +2,48 @@ import { promises } from 'fs'
 import { join } from 'path'
 import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
-import axios from 'axios';
-
-let Styles = (text, style = 1) => {
-  var xStr = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
-  var yStr = Object.freeze({
-    1: 'ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxʏᴢ1234567890'
-  });
-  var replacer = [];
-  xStr.map((v, i) => replacer.push({
-    original: v,
-    convert: yStr[style].split('')[i]
-  }));
-  var str = text.toLowerCase().split('');
-  var output = [];
-  str.map(v => {
-    const find = replacer.find(x => x.original == v);
-    find ? output.push(find.convert) : output.push(v);
-  });
-  return output.join('');
-};
 
 let tags = {
-  'pago': '💲 ᴘʀᴇᴄɪᴏ ᴛᴀɴᴊɪʀᴏ 💲',
-  'main': '✨ ɪɴꜰᴏ ʙᴏᴛ ✨',
-  'menu': '👑 ᴍᴇɴᴜ 👑',
-  'buscador': '🔎 ʙᴜꜱQᴜᴇᴅᴀꜱ 🔎',
-  'search': '🔍 ꜱᴇᴀʀᴄʜ 🔍',
-  'rg': '🛡️ ʀᴇɢɪꜱᴛʀᴏ 🛡️',
-  'sticker': '💢 ꜱᴛɪᴄᴋᴇʀꜱ 💢',
-  'database': '💥ᴅᴀᴛᴀʙᴀꜱᴇ💥',
-  'fix': ', ✋🏼 ꜰɪxᴍꜱɢᴇꜱᴘᴇʀᴀ ✋🏼',
-  'grupo': '👥 ɢʀᴜᴘᴏꜱ 👥',
-  'nable': '👀 ᴏɴ / ᴏꜰꜰ 👀', 
-  'dl': '🎵 ᴅᴇꜱᴄᴀʀɢᴀꜱ 🎵',
-  'info': '⚔️ ɪɴꜰᴏʀᴍᴀᴄɪᴏɴ ⚔️',
-  'nsfw': '🔞 ɴꜱꜰᴡ 🔞', 
-  'owner': '👑 ᴄʀᴇᴀᴅᴏʀ 👑',
-  'mods': '👤 ꜱᴛᴀꜰꜰ 👤',
-  'ai': '🔰 ᴀɪ ʙᴏᴛ 🔰',
-  'convertir': '⭕ ᴄᴏɴᴠᴇʀᴛɪᴅᴏʀᴇꜱ ⭕',
+  'main': 'Info',
+  'serbot': 'Sub Bots',
+  'rpg': 'RPG',
+  'rg': 'Registro',
+  'sticker': 'Stickers',
+  'img': 'Imágenes',
+  'group': 'Grupos',
+  'logo': 'Logo',
+  'nable': 'On/Off', 
+  'tools': 'Herramientas',
+  'nsfw': 'Nsfw', 
+  'owner': 'Creador', 
+  'advanced': 'Avanzado',
+  '+18': '+18',
   
 }
 
 const defaultMenu = {
-  before: `Hola \`%name\` soy ᴋᴜʀᴜᴍɪᏴo͢Ꭲ, %greeting
-
-乂 _\`ᴜ\` \`ꜱ\` \`ᴜ\` \`ᴀ\` \`ʀ\` \`ɪ\` \`ᴏ\`_ 乂
-
-• _\`ɴᴏᴍʙʀᴇ\`_ :: %name
-• _\`ʙᴏᴛ\`_ :: ᴋᴜʀᴜᴍɪᏴo͢Ꭲ
-• _\`ᴍᴏᴅᴏ\`_ :: Público
-• _\`ᴀᴄᴛɪᴠᴏ\`_ :: %muptime
-• _\`ᴜꜱᴜᴀʀɪᴏꜱ\`_ :: %totalreg
-
-乂 _\`ᴄ\` \`ᴏ\` \`ᴍ\` \`ᴀ\` \`ɴ\` \`ᴅ\` \`ᴏ\` \`ꜱ\`_ 乂
+  before: `  
+ঔৣ͜͡SonGokuBOTꦽꦼᬏ
+⸼݇҉ֻ᠂⃟🐉─➤Github: github.com/YerTX2/SonGOKU 
+╭──────────────────✎
+╰─➤INFO
+│㆒⸼݇҉ֻ᠂⃟🐉KI : %limit
+│㆒⸼݇҉ֻ᠂⃟🐉Exp : %totalexp
+│㆒⸼݇҉ֻ᠂⃟🐉Nivel : %level
+╰─────➤☆ۣۜۜ͜͡%name𖣘⃟ᗒ  
+ ㆒⸼݇҉ֻ᠂⃟🐉ACTIVO: %uptime 
+ %readmore
 `.trimStart(),
-  header: '╭─(❀)❝┊ *_`%category`_* ┊❝(❀)',
-  body: '┊➧ %cmd\n',
-  footer: '╰───────────── –\n',
-  after: `> ᴋᴜʀᴜᴍɪᏴo͢Ꭲ`,
+  header: '`✧͜͡҉MENU %category⛤⃗͜ᬏ᭄`\n\n┌─⋅☆·̇·̣̇̇·̣̣̇·̣̇̇·̇⸼݇҉ֻ᠂⃟🐉୨୧┈┈┈୨୧⸼݇҉ֻ᠂⃟🐉·̇·̣̇̇·̣̣̇·̣̇̇☆─⋅┐',
+  body: '│ ⋆ ҈͜͡➳ %cmd*\n',
+  footer: '└─⋅☆·̇·̣̇̇·̣̣̇·̣̇̇·̇⸼݇҉ֻ᠂⃟🐉୨୧┈┈┈୨୧⸼݇҉ֻ᠂⃟🐉·̇·̣̇̇·̣̣̇·̣̇̇☆─⋅┘\n',
+  after: '',
 }
-let ppp = 'https://qu.ax/CkXP.jpg'
+
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
     let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-    let { exp, corazones, level, role } = global.db.data.users[m.sender]
+    let { exp, limit, level } = global.db.data.users[m.sender]
     let { min, xp, max } = xpRange(level, global.multiplier)
     let name = await conn.getName(m.sender)
     let d = new Date(new Date + 3600000)
@@ -107,7 +83,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
         help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
         tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
         prefix: 'customPrefix' in plugin,
-        corazones: plugin.corazones,
+        limit: plugin.limit,
         premium: plugin.premium,
         enabled: !plugin.disabled,
       }
@@ -121,7 +97,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let header = conn.menu.header || defaultMenu.header
     let body = conn.menu.body || defaultMenu.body
     let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == conn.user.jid ? '' : `Powered by https://wa.me/${conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : ``) + defaultMenu.after
     let _text = [
       before,
       ...Object.keys(tags).map(tag => {
@@ -129,8 +105,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
           ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
             return menu.help.map(help => {
               return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%isdiamond/g, menu.diamond ? '◜🪙◞' : '')
-                .replace(/%isPremium/g, menu.premium ? '◜🎫◞' : '')
+                .replace(/%islimit/g, menu.limit ? '' : '')
+                .replace(/%isPremium/g, menu.premium ? '' : '')
                 .trim()
             }).join('\n')
           }),
@@ -140,59 +116,44 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       after
     ].join('\n')
     let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
-let replace = {
-'%': '%',
-p: _p, uptime, muptime,
-me: conn.getName(conn.user.jid),
-taguser: '@' + m.sender.split("@s.whatsapp.net")[0],
-npmname: _package.name,
-npmdesc: _package.description,
-version: _package.version,
-exp: exp - min,
-maxexp: xp,
-botofc: (conn.user.jid == global.conn.user.jid ? '🌸 𝙴𝚂𝚃𝙴 𝙴𝚂 𝙴𝙻 𝙱𝙾𝚃 𝙾𝙵𝙲' : `🌸 𝚂𝚄𝙱-𝙱𝙾𝚃 𝙳𝙴: Wa.me/${global.conn.user.jid.split`@`[0]}`), 
-totalexp: exp,
-xp4levelup: max - exp,
-github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
-greeting, level, corazones, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
-readmore: readMore
-}
-text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-
-const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-
-const pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://i.ibb.co/S0m6f3D/file.jpg')
-
-  let category = "video"
-  const db = './media/database/db.json'
-  const db_ = JSON.parse(fs.readFileSync(db))
-  const random = Math.floor(Math.random() * db_.links[category].length)
-  const rlink = db_.links[category][random]
-  global.vid = rlink
-  const response = await fetch(vid)
-  const gif = await response.buffer()
- // const img = imagen1
-
-await m.react('🇦🇱') 
-// await conn.reply(m.chat, '*ꪹ͜𓂃͡𝗖𝗮𝗿𝗴𝗮𝗻𝗱𝗼 𝗘𝗹 𝗠𝗲𝗻𝘂 𝗗𝗲 𝗹𝗮 𝗕𝗼𝘁...𓏲੭*', fakegif3, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: packname, body: '複|ᴋᴜʀᴜᴍɪᏴo͢Ꭲツ', sourceUrl: canal, thumbnail: icons }}})
-
-// await conn.reply(m.chat, '🍟 Enviando el menú.....', m, rcanal)
-let imagen_menu = await getBuffer(ppp);
-await conn.sendFile(m.chat, imagen_menu, 'menu.jpg', Styles(text.trim()), fakegif3, null, fake)
-
-/* await conn.sendButton(m.chat, text, '@usxr_angelito0', ppp, [
-['', '']], null, [['CANAL 🐈‍⬛', `${canal}`], ['CANAL 2', `wa.me/51 907 376 960`]], m) */
+    let replace = {
+      '%': '%',
+      p: _p, uptime, muptime,
+      taguser: '@' + m.sender.split("@s.whatsapp.net")[0],
+      wasp: '@0',
+      me: conn.getName(conn.user.jid),
+      npmname: _package.name,
+      version: _package.version,
+      npmdesc: _package.description,
+      npmmain: _package.main,
+      author: _package.author.name,
+      license: _package.license,
+      exp: exp - min,
+      maxexp: xp,
+      totalexp: exp,
+      xp4levelup: max - exp,
+      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
+      level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg,
+      readmore: readMore
+    }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    
+let img = await (await fetch(`https://i.ibb.co/6X35QcR/file.jpg`)).buffer()
+    
+   // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
+    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
+   //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
 
   } catch (e) {
-    conn.reply(m.chat, '⚠️ Lo sentimos, el menú tiene un error', m, rcanal, )
+    conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
     throw e
   }
 }
-handler.help = ['allmenu']
-handler.tags = ['menu']
-handler.command = ['menuall', 'allmenú', 'allmenu'] 
-handler.register = true
 
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['menu', 'help','goku', 'menú'] 
+handler.register = true 
 export default handler
 
 const more = String.fromCharCode(8206)
@@ -204,51 +165,3 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
-
-  var ase = new Date();
-  var hour = ase.getHours();
-switch(hour){
-  case 0: hour = 'Buenas noches 🌙'; break;
-  case 1: hour = 'Buenas noches 💤'; break;
-  case 2: hour = 'Buenas noches 🦉'; break;
-  case 3: hour = 'Buenas noches ✨'; break;
-  case 4: hour = 'Buenos dias 👾'; break;
-  case 5: hour = 'Buenos dias 🌅'; break;
-  case 6: hour = 'Buenos dias 🌄'; break;
-  case 7: hour = 'Buenos dias 🌅'; break;
-  case 8: hour = 'Buenos dias 👾'; break;
-  case 9: hour = 'Buenos dias ✨'; break;
-  case 10: hour = 'Buenos dias 🌞'; break;
-  case 11: hour = 'Buenos dias 🌨'; break;
-  case 12: hour = 'Buenos dias ❄'; break;
-  case 13: hour = 'Buenos dias 🌤'; break;
-  case 14: hour = 'Buenas tardes 🌇'; break;
-  case 15: hour = 'Buenas tardes 🥀'; break;
-  case 16: hour = 'Buenas tardes 🌹'; break;
-  case 17: hour = 'Buenas tardes 🌆'; break;
-  case 18: hour = 'Buenas noches 🌙'; break;
-  case 19: hour = 'Buenas noches 🌃'; break;
-  case 20: hour = 'Buenas noches 🌌'; break;
-  case 21: hour = 'Buenas noches 🌃'; break;
-  case 22: hour = 'Buenas noches 🌙'; break;
-  case 23: hour = 'Buenas noches 🌃'; break;
-}
-  var greeting = hour;
-
-/*const getBuffer = async (url, options) => {
-try {
-const res = await axios({
-method: 'get',
-url,
-headers: {
-'DNT': 1,
-'Upgrade-Insecure-Request': 1,
-},
-...options,
-responseType: 'arraybuffer',
-});
-return res.data;
-} catch (e) {
-console.log(`Error : ${e}`);
-}
-};*/
