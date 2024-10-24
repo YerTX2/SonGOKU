857eb42cf9b1e413ba4db87eb4f098975f02f96a
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-10-24 00:09:02 -0500

    Actualizar descarga-tiktok.js

--- plugins/descarga-tiktok.js
+++ plugins/descarga-tiktok.js
@@ -3,7 +3,7 @@ import Starlights from '@StarlightsTeam/Scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args || !args[0]) return conn.reply(m.chat, '🚩 Ingresa un enlace del vídeo de TikTok junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://vm.tiktok.com/ZMrFCX5jf/`, m, rcanal)
     if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `Verifica que el link sea de TikTok`, m, rcanal).then(_ => m.react('✖️'))
-  await m.react('👾')
+  await m.react('🕓')
 try {
 let { title, author, duration, views, likes, comment, share, published, downloads, dl_url } = await Starlights.tiktokdl(args[0])
 let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n'
@@ -22,3 +22,9 @@ await m.react('✅')
 } catch {
 await m.react('✖️')
 }}
+handler.help = ['tiktok *<url tt>*']
+handler.tags = ['downloader']
+handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
+handler.register = true
+
+export default handler
