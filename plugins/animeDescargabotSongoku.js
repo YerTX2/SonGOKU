b590322070d15654422a21c0d2ceaaa80cdf7449
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-03 16:42:00 -0500

    Actualizar animeDescargabotSongoku.js

--- plugins/animeDescargabotSongoku.js
+++ plugins/animeDescargabotSongoku.js
@@ -56,6 +56,6 @@ handler.help = ['animedl <anime-id> <episode-number>'];
 handler.tags = ['downloader'];
 handler.command = ['animedl', 'animeflvdl', 'anidl'];
 handler.group = true
-handler.limit = 3
+handler.limit = 5
 
 export default handler;
