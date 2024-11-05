2b89aa5d989eb2e6c04486f39aead0d226144c7b
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-05 03:14:33 -0500

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -7,7 +7,7 @@ let img = catalogo
     contextInfo: {
     	isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363314192605628@newsletter",
+      newsletterJid: "120363350628883149@newsletter",
       serverMessageId: 100,
       newsletterName: 'ƬΣΛM ΛПG',
     },
