0b06866cf2aebed4e3a853b7d7da7ee30b1fcaed
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-05 00:54:00 -0500

    Actualizar rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -3,7 +3,7 @@ let handler = async (m, {conn, usedPrefix}) => {
    if (who == conn.user.jid) return m.react('✖️')
    if (!(who in global.db.data.users)) return m.reply(`*El usuario no se encuentra en mi base de datos*`)
    let user = global.db.data.users[who]
-   await m.reply(`${who == m.sender ? `Tienes *${user.bank} ⭐ Estrellas* en el Banco` : `El usuario @${who.split('@')[0]} tiene *${user.bank} ⭐ Estrellas* en el Banco`}`, null, { mentions: [who] })
+   await m.reply(`${who == m.sender ? `Tienes *${user.bank} Ki⚡* en el Banco` : `El usuario @${who.split('@')[0]} tiene *${user.bank} ki ⚡* en el Banco`}`, null, { mentions: [who] })
 }
 
 handler.help = ['bank']
