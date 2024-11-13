9fe25ef85015e6d95ebada2cbe85972794a2de84
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-13 14:29:24 -0500

    Actualizar plugins

---
+++ plugins/YouTube-play2-SonGoku.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+import yts from 'yt-search';
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+  if (!text) throw m.reply(`Ejemplo de uso: ${usedPrefix + command} Joji Ew`);
+  
+    let results = await yts(text);
+    let tes = results.videos[0]
+    
+const baseUrl = 'https://cuka.rfivecode.com';
+const cukaDownloader = {
+  youtube: async (url, exct) => {
+    const format = [ 'mp3', 'mp4' ];
+    try {
+      const response = await fetch(`${baseUrl}/download`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+          body: JSON.stringify({ url, format: exct })
+      });
+
+      const data = await response.json();
+      return data;
+      console.log('Data:' + data);
+    } catch (error) {
+      return { success: false, message: error.message };
+      console.error('Error:', error);
+    }
+  },
+  tiktok: async (url) => {
+    try {
+      const response = await fetch(`${baseUrl}/tiktok/download`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+          body: JSON.stringify({ url })
+      });
+
+      const data = await response.json();
+      return data;
+      console.log('Data:' + data);
+    } catch (error) {
+      return { success: false, message: error.message };
+      console.error('Error:', error);
+    }
+  },
+  spotify: async (url) => {
+    try {
+      const response = await fetch(`${baseUrl}/spotify/download`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+          body: JSON.stringify({ url })
+      });
+
+      const data = await response.json();
+      return data;
+      console.log('Data:' + data);
+    } catch (error) {
+      return { success: false, message: error.message };
+      console.error('Error:', error);
+    }
+  }
+}
+
+let dataos = await cukaDownloader.youtube(tes.url, "mp3")
+console.log(dataos)
+let { title, thumbnail, quality, downloadUrl } = dataos
+  m.reply(`_✧ Enviando ${title} (${quality})_\n\n> ${tes.url}`)
+      const doc = {
+      audio: { url: downloadUrl },
+      mimetype: 'audio/mp4',
+      fileName: `${title}.mp3`,
+      contextInfo: {
+        externalAdReply: {
+          showAdAttribution: true,
+          mediaType: 2,
+          mediaUrl: tes.url,
+          title: title,
+          sourceUrl: tes.url,
+          thumbnail: await (await conn.getFile(thumbnail)).data
+        }
+      }
+    };
+    await conn.sendMessage(m.chat, doc, { quoted: m });
+}
+handler.help = ['play2'];
+handler.tags = ['downloader'];
+handler.command = /^(play2)$/i;
+
+export default handler;
