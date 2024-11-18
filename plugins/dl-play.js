ee06b65a67d7d7b0708116f84b2a258dd987e6f2
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-17 20:59:40 -0500

    Actualizar dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -49,9 +49,9 @@ let handler = async (m, { conn, command, usedPrefix, args }) => {
   }
 }
 
-handler.help = ["play <consulta>"]
-handler.tags = ["downloader"]
-handler.command = /^(play|ytplay|playmp3|song)$/i
+//handler.help = ["play <consulta>"]
+//handler.tags = ["downloader"]
+//handler.command = /^(play|ytplay|playmp3|song)$/i
 handler.limit = 3
 export default handler
 
