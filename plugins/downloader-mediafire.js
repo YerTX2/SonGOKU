49d24b21904c7d3b53a6105a6ad1cad760e0df47
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-18 01:19:49 -0500

    Actualizar downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -5,7 +5,7 @@ if (!args[0]) return m.reply('🌊 Ingresa el enlace del archivo de Mediafire ju
 if (!args[0].match(/mediafire/gi)) return m.reply('El enlace deve ser de un archivo de Mediafire.')
 try {
 let { title, ext, aploud, size, dl_url } = await Scraper.mediafire(args[0])
-if (size.includes('GB') || size.replace(' MB', '') > 300) { return await m.reply('El archivo pesa mas de 300 MB, se canceló la Descarga.')}
+if (size.includes('GB') || size.replace(' MB', '') > 2000) { return await m.reply('El archivo pesa mas de 2000 MB, se canceló la Descarga.')}
 let txt = `╭─⬣「 *Mediafire Download* 」⬣\n`
     txt += `│  ≡◦ *🧿 Nombre ∙* ${title}\n`
     txt += `│  ≡◦ *🪴 Subido ∙* ${aploud}\n`
