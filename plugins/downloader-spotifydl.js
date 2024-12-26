a10afee76481ea07d836fc2803ad23ae09dc6aef
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-12-26 02:31:34 -0500

    Actualizar downloader-spotifydl.js

--- plugins/downloader-spotifydl.js
+++ plugins/downloader-spotifydl.js
@@ -1,31 +1,28 @@
-import Starlights from '@StarlightsTeam/Scraper';
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '🚩 Ingresa el enlace de algún Track, Playlist o Álbum de Spotify.', m);
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+if (!args[0]) return m.reply(`Ingresa un enlace de Spotify`)
 
-let isSpotifyUrl = text.match(/^(https:\/\/open\.spotify\.com\/(album|track|playlist)\/[a-zA-Z0-9]+)/i);
-if (!isSpotifyUrl) return conn.reply(m.chat, '🚩 Ingresa un enlace válido de Track, Playlist o Álbum de Spotify.', m);
-
-await m.react('🕓')
 try {
-let { title, artist, album, thumbnail, dl_url } = await Starlights.spotifydl(text);
-let img = await (await fetch(thumbnail)).buffer();
+let api = await fetch(`https://deliriussapi-oficial.vercel.app/download/spotifydlv2?url=${args[0]}`)
+let json = await api.json()
+let { data } = json
+let { title, artist, image, cover, url, album, duration, publish, popularity, preview, download } = data
 
-let txt = `*乂  S P O T I F Y  -  D O W N L O A D*\n\n`;
-    txt += `    ✩  *Título* : ${title}\n`;
-    txt += `    ✩  *Álbum* : ${album}\n`;
-    txt += `    ✩  *Artista* : ${artist}\n\n`;
-    txt += `*- ↻ Los audios se están enviando, espera un momento. . .*`;
+let JT = `*Titulo:* ${title}
+*autor:* ${artist}
+*Album :* ${album}
+*Duracion :* ${duration}
+*Publicado :* ${publish}
+*Popularidad :* ${popularity}`
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal);
-await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m });
 
-await m.react('✅');
-} catch {
-await m.react('✖️');
-}
-};
+await conn.sendFile(m.chat, image, `HasumiBotFreeCodes.jpeg`, JT, m);
+await conn.sendFile(m.chat, download, 'hasumiBotFreeCodes.mp3', null, m)
+
+} catch (error) {
+console.error(error)
+}}
 
 
 handler.command = /^(spotifydl)$/i
