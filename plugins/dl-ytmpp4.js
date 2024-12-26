714f3eb8bcb6b355e9bf4d7cf66591d4d742c01e
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-12-26 13:52:35 -0300

    Actualizar dl-ytmpp4.js

--- plugins/dl-ytmpp4.js
+++ plugins/dl-ytmpp4.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, `❀ Ingresa un  link de youtube`, m, rcanal)
     
 try {
-let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json()
+let api = await (await fetch(`https://deliriussapi-oficial.vercel.app/download/ytmp4?url=${text}`)).json()
 let dl_url = api.data.dl
 
 await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m })
