57a311e77e47199973522963ab0178740403d6cd
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-12-26 23:05:14 -0500

    Actualizar serbot-code.js

--- plugins/serbot-code.js
+++ plugins/serbot-code.js
@@ -1,14 +1,23 @@
-const { DisconnectReason, useMultiFileAuthState, MessageRetryMap, fetchLatestBaileysVersion, Browsers, makeCacheableSignalKeyStore, jidNormalizedUser, PHONENUMBER_MCC } = await import('@whiskeysockets/baileys')
+const {
+    useMultiFileAuthState,
+    DisconnectReason,
+    fetchLatestBaileysVersion, 
+    MessageRetryMap,
+    makeCacheableSignalKeyStore, 
+    jidNormalizedUser,
+    PHONENUMBER_MCC
+   } = await import('@whiskeysockets/baileys')
 import moment from 'moment-timezone'
 import NodeCache from 'node-cache'
 import readline from 'readline'
 import qrcode from "qrcode"
+import crypto from 'crypto'
 import fs from "fs"
-import pino from 'pino'
-import * as ws from 'ws'
+import pino from 'pino';
+import * as ws from 'ws';
 const { CONNECTING } = ws
 import { Boom } from '@hapi/boom'
-import { makeWASocket } from '../lib/simple.js'
+import { makeWASocket } from '../lib/simple.js';
 
 if (global.conns instanceof Array) console.log()
 else global.conns = []
@@ -21,80 +30,75 @@ let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner }) => 
 
   async function serbot() {
 
-  let authFolderB = m.sender.split('@')[0]
+  let authFolderB = crypto.randomBytes(10).toString('hex').slice(0, 8)
 
     if (!fs.existsSync("./serbot/"+ authFolderB)){
         fs.mkdirSync("./serbot/"+ authFolderB, { recursive: true });
     }
     args[0] ? fs.writeFileSync("./serbot/" + authFolderB + "/creds.json", JSON.stringify(JSON.parse(Buffer.from(args[0], "base64").toString("utf-8")), null, '\t')) : ""
 
-    const {state, saveState, saveCreds} = await useMultiFileAuthState(`./serbot/${authFolderB}`)
-    const msgRetryCounterMap = (MessageRetryMap) => { }
-    const msgRetryCounterCache = new NodeCache()
-    const {version} = await fetchLatestBaileysVersion()
-    let phoneNumber = m.sender.split('@')[0]
+const {state, saveState, saveCreds} = await useMultiFileAuthState(`./serbot/${authFolderB}`)
+const msgRetryCounterMap = (MessageRetryMap) => { };
+const msgRetryCounterCache = new NodeCache()
+const {version} = await fetchLatestBaileysVersion();
+let phoneNumber = m.sender.split('@')[0]
 
-    const methodCodeQR = process.argv.includes("qr")
-    const methodCode = !!phoneNumber || process.argv.includes("code")
-    const MethodMobile = process.argv.includes("mobile")
+const methodCodeQR = process.argv.includes("qr")
+const methodCode = !!phoneNumber || process.argv.includes("code")
+const MethodMobile = process.argv.includes("mobile")
 
-    const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
-    const question = (texto) => new Promise((resolver) => rl.question(texto, resolver))
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+const question = (texto) => new Promise((resolver) => rl.question(texto, resolver))
 
-    const connectionOptions = {
-      logger: pino({ level: 'silent' }),
-      printQRInTerminal: false,
-      mobile: MethodMobile, 
-      browser: [ "Ubuntu", "Chrome", "20.0.04"], 
-      auth: {
-        creds: state.creds,
-        keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
-      },
-      markOnlineOnConnect: true, 
-      generateHighQualityLinkPreview: true, 
-      getMessage: async (clave) => {
-        let jid = jidNormalizedUser(clave.remoteJid)
-        let msg = await store.loadMessage(jid, clave.id)
-        return msg?.message || ""
-      },
-      msgRetryCounterCache,
-      msgRetryCounterMap,
-      defaultQueryTimeoutMs: undefined,   
-      version
+const connectionOptions = {
+  logger: pino({ level: 'silent' }),
+  printQRInTerminal: false,
+  mobile: MethodMobile, 
+  browser: [ "Ubuntu", "Chrome", "20.0.04" ], 
+  auth: {
+  creds: state.creds,
+  keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+  },
+  markOnlineOnConnect: true, 
+  generateHighQualityLinkPreview: true, 
+  getMessage: async (clave) => {
+  let jid = jidNormalizedUser(clave.remoteJid)
+  let msg = await store.loadMessage(jid, clave.id)
+  return msg?.message || ""
+  },
+  msgRetryCounterCache,
+  msgRetryCounterMap,
+  defaultQueryTimeoutMs: undefined,   
+  version
+  }
+
+let conn = makeWASocket(connectionOptions)
+
+if (methodCode && !conn.authState.creds.registered) {
+    if (!phoneNumber) {
+        process.exit(0);
+    }
+    let cleanedNumber = phoneNumber.replace(/[^0-9]/g, '');
+    if (!Object.keys(PHONENUMBER_MCC).some(v => cleanedNumber.startsWith(v))) {
+        process.exit(0);
     }
 
-    let conn = makeWASocket(connectionOptions)
-
-    if (methodCode && !conn.authState.creds.registered) {
-        if (!phoneNumber) {
-            process.exit(0)
-        }
-        let cleanedNumber = phoneNumber.replace(/[^0-9]/g, '')
-        if (!Object.keys(PHONENUMBER_MCC).some(v => cleanedNumber.startsWith(v))) {
-            process.exit(0)
-        }
-
-        setTimeout(async () => {
-            let codeBot = await conn.requestPairingCode(cleanedNumber)
-            codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot
-            let txt = `✿ *Vincula tu cuenta usando el codigo.*\n\n`
-                txt += `[ ✰ ] Sigue las instrucciones:\n`
-                txt += `*» Mas opciones*\n`
-                txt += `*» Dispositivos vinculados*\n`
-                txt += `*» Vincular nuevo dispositivo*\n`
-                txt += `*» Vincular usando numero*\n\n`
-                txt += `> *Nota:* Este Código solo funciona en el número que lo solicito`
-            let pp = "./storage/mp4/serbot.mp4"
-            let sendTxt = await star.reply(m.chat, txt, m, rcanal)
-            let sendCode = await star.reply(m.chat, codeBot, m, rcanal)
-        
-            setTimeout(() => {
-                star.sendMessage(m.chat, { delete: sendTxt })
-                star.sendMessage(m.chat, { delete: sendCode })
-            }, 30000)
-            rl.close()
-        }, 3000)
-    }
+    setTimeout(async () => {
+        let codeBot = await conn.requestPairingCode(cleanedNumber);
+        codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot;
+        let txt = ` –  *S E R B O T  -  S U B B O T*\n\n`
+            txt += `┌  ✩  *Usa este Código para convertirte en un Sub Bot*\n`
+            txt += `│  ✩  Pasos\n`
+            txt += `│  ✩  *1* : Haga click en los 3 puntos\n`
+            txt += `│  ✩  *2* : Toque dispositivos vinculados\n`
+            txt += `│  ✩  *3* : Selecciona *Vincular con el número de teléfono*\n` 
+            txt += `└  ✩  *4* : Escriba el Codigo\n\n`
+            txt += `*Nota:* Este Código solo funciona en el número que lo solicito`
+         await parent.reply(m.chat, txt, m, rcanal)
+         await parent.reply(m.chat, codeBot, m, rcanal)
+        rl.close()
+    }, 3000)
+}
 
 conn.isInit = false
 let isInit = true
@@ -131,20 +135,15 @@ async function connectionUpdate(update) {
  
   }
 
-  const timeoutId = setTimeout(() => {
-        if (!conn.user) {
-            try {
-                conn.ws.close()
-            } catch {}
-            conn.ev.removeAllListeners()
-            let i = global.conns.indexOf(conn)
-            if (i >= 0) {
-                delete global.conns[i]
-                global.conns.splice(i, 1)
-            }
-            fs.rmdirSync(`./serbot/${authFolderB}`, { recursive: true })
-        }
-    }, 30000)
+  setInterval(async () => {
+    if (!conn.user) {
+      try { conn.ws.close() } catch { }
+      conn.ev.removeAllListeners()
+      let i = global.conns.indexOf(conn)
+      if (i < 0) return
+      delete global.conns[i]
+      global.conns.splice(i, 1)
+    }}, 60000)
 	
 let handler = await import('../handler.js')
 let creloadHandler = async function (restatConn) {
@@ -190,5 +189,5 @@ handler.rowner = false
 export default handler
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
