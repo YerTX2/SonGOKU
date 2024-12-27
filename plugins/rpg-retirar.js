b013ac0fd8a27b04df060c77f9317e5ce1aa2b2c
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-12-27 20:16:09 -0300

    Actualizar rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -1,24 +1,26 @@
 let handler = async (m, { args }) => {
-   let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *KI ⚡* que deseas Retirar.')
-   if (args[0] == 'all') {
-      let count = parseInt(user.bank)
-      user.bank -= count * 1
-      user.limit += count * 1
-      await m.reply(`🚩 Retiraste *${count} KI ⚡* del Banco.`)
-      return !0
-   }
-   if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
-   let count = parseInt(args[0])
-   if (!user.bank) return m.reply('No tienes * KI ⚡ * en el Banco.')
-   if (user.bank < count) return m.reply(`Solo tienes *${user.bank} KI ⚡*  en el Banco.`)
-   user.bank -= count * 1
-   user.limit += count * 1
-   await m.reply(`🚩 Retiraste *${count} KI ⚡* del Banco.`)
-}
+    let user = global.db.data.users[m.sender];
+    if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *KI ⚡* que deseas retirar.');
+    if (args[0].toLowerCase() === 'all') {
+        if (user.bank < 1) return m.reply('🚩 No tienes *KI ⚡* en el Banco para retirar.');
+        let count = user.bank;
+        user.bank = 0;
+        user.limit += count;
+        await m.reply(`🚩 Retiraste *${count} KI ⚡* del Banco.`);
+        return !0;
+    }
+    if (isNaN(args[0]) || Number(args[0]) < 1) {
+        return m.reply('🚩 Ingresa una cantidad válida de *KI ⚡*.');
+    }
+    let count = parseInt(args[0]);
+    if (user.bank < count) return m.reply(`🚩 Solo tienes *${user.bank} KI ⚡* en el Banco.`);
+    user.bank -= count;
+    user.limit += count;
+    await m.reply(`🚩 Retiraste *${count} KI ⚡* del Banco.`);
+};
 
-handler.help = ['retirar']
-handler.tags = ['rpg']
-handler.command = ['withdraw', 'retirar', 'wd']
-handler.register = true 
-export default handler
+handler.help = ['retirar'];
+handler.tags = ['rpg'];
+handler.command = ['withdraw', 'retirar', 'wd'];
+handler.register = true;
+export default handler;
