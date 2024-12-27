9078fd29ee1e4deba9d0d7cf4d8811e97c60156f
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-12-27 02:57:00 -0300

    Actualizar herramientas-chagpt.js

--- plugins/herramientas-chagpt.js
+++ plugins/herramientas-chagpt.js
@@ -56,8 +56,8 @@ let res = await gpt.json()
 await m.reply(res.message)
 }
 
-if (command == 'bing' || command == 'bing') {
-let gpt = await fetch(`https://deliriussapi-oficial.vercel.app/ia/bingia?query=${text}`)
+if (command == 'pixai' || command == 'pixai') {
+let gpt = await fetch(`https://api.dorratz.com/v2/pix-ai?prompt=${text}`)
 let res = await gpt.json()
 await m.reply(res.message)
 }}
