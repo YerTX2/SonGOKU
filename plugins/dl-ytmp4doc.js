a884f09119447e4f09cca921db7a6aeb01c5e632
ZxYerSixZx <oroscoyerson2019@gmail.com>
2025-01-05 02:53:35 -0500

    Actualizar dl-ytmp4doc.js

--- plugins/dl-ytmp4doc.js
+++ plugins/dl-ytmp4doc.js
@@ -1,3 +1,19 @@
+//Derechos del código de 
+/* 
+*❀ By Jtxs*
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
 //Código modificado por YerTX2 🇦🇱para que haga descargar en DOCUMENTO NO SEAS RATA NO BORRES 🦁
 
 
