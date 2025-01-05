cb414f5c8d930898e1b095e57b9c68e0d805c7f8
ZxYerSixZx <oroscoyerson2019@gmail.com>
2025-01-05 02:53:05 -0500

    Actualizar dl-ytmpp4.js

--- plugins/dl-ytmpp4.js
+++ plugins/dl-ytmpp4.js
@@ -1,3 +1,20 @@
+//Derechos del código de 
+/* 
+*❀ By Jtxs*
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
+
 //Código modificado por YerTX2 🇦🇱 con advertencias de descarga 
 
 import fetch from 'node-fetch';
