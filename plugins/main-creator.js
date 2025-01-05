62c4e012c8cfeb2574c8357f60c723e47bc577d8
ZxYerSixZx <oroscoyerson2019@gmail.com>
2025-01-05 00:17:34 -0500

    Delete plugins directory

--- plugins/main-creator.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:YerTX2;;\nFN:YerTX2 \nORG:YerTX2 🌟\nTITLE:\nitem1.TEL;waid=51907376960:51907376960\nitem1.X-ABLabel:YerTX2 🌟\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:YerTX2  🌟\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'YerTX2.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-export default handler
--- plugins/owner-darpremium.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { isOwner, reply }) => {
-    if (!isOwner) return reply('Solo el propietario puede usar este comando.');
-    let user = global.db.data.users[m.sender];
-    user.prem = true;
-    user.premiumTime = Date.now() + 7 * 24 * 60 * 60 * 1000; 
-    m.reply('El Usuario ahora es premium por 7 días');
-};
-handler.command = ['darpremium'];
-export default handler;
