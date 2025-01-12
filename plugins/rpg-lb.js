f1748718a6b8a3206ba720a80ba60e965823c3d2
ZxYerSixZx <oroscoyerson2019@gmail.com>
2025-01-12 00:31:22 -0500

    Actualizar rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -1,3 +1,5 @@
+
+
 let handler = async (m, { conn, args, participants }) => {
 let users = Object.entries(global.db.data.users).map(([key, value]) => {
 return {...value, jid: key}})
@@ -8,32 +10,32 @@ let usersExp = sortedExp.map(enumGetKey)
 let usersLim = sortedLim.map(enumGetKey) 
 let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
-  let text = `
-╭───═[ *Top ${len} KI ⚡* ]═────⋆
-│╭───────────────···
-✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩│ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} ⚡*`).join`\n✩│ `}
-│╰────────────────···
-╰───────────═┅═──────────
+let text = `
+🔥 *RANKING Z - * 🔥
+━━━━━━━━━━━━━━━━━━━━━━━
+🏆 *TOP ${len} KI ⚡* 🏆
+${sortedLim.slice(0, len).map(({ jid, limit }, i) => 
+`   ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)})` : '@'}${jid.split`@`[0]} - *${limit}⚡*`).join('\n')}
+💥 *Tu Nivel KI*: ${usersLim.indexOf(m.sender) + 1}/${usersLim.length}
+━━━━━━━━━━━━━━━━━━━━━━━
 
-╭───═[ *TOP ${len} XP 💫* ]═────⋆
-│╭───────────────···
-✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩│ ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} 💫*`).join`\n✩│ `}
-│╰────────────────···
-╰───────────═┅═──────────
+🌌 *TOP ${len} XP - PODER TOTAL 💫* 🌌
+${sortedExp.slice(0, len).map(({ jid, exp }, i) => 
+`   ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)})` : '@'}${jid.split`@`[0]} - *${exp}💫*`).join('\n')}
+✨ *Tu Experiencia*: ${usersExp.indexOf(m.sender) + 1}/${usersExp.length}
+━━━━━━━━━━━━━━━━━━━━━━━
 
-╭───═[ *Top ${len} Nivel 📈* ]═────⋆
-│╭───────────────···
-✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩│ ${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Nivel ${level} 📈*`).join`\n✩│ `}
-│╰────────────────···
-╰───────────═┅═──────────`.trim()
-  m.reply(text, null, { mentions: conn.pa rseMention(text) })
+⚡ *TOP ${len} NIVELES - MÁS ALLÁ DEL LÍMITE 📈* ⚡
+${sortedLevel.slice(0, len).map(({ jid, level }, i) => 
+`   ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)})` : '@'}${jid.split`@`[0]} - *Nivel ${level}📈*`).join('\n')}
+🌟 *Tu Nivel Actual*: ${usersLevel.indexOf(m.sender) + 1}/${usersLevel.length}
+━━━━━━━━━━━━━━━━━━━━━━━
+
+ *¡DESAFÍA TUS LÍMITES Y CONVIÉRTETE EN EL MÁS FUERTE!* 
+`.trim();
+
+  m.reply(text, null, { mentions: conn.parseMention(text) })
 }
-
-m.reply(text, null, { mentions: conn.parseMention(text) });
-
 handler.help = ['lb']
 handler.tags = ['rpg']
 handler.command = ['leaderboard', 'lb'] 
@@ -49,12 +51,4 @@ function sort(property, ascending = true) {
 }
 
 function toNumber(property, _default = 0) {
-  if (property) return (a, i, b) => {
-    return {...b[i], [property]: a[property] === undefined ? _default : a[property]}
-  }
-  else return a => a === undefined ? _default : a
-}
-
-function enumGetKey(a) {
-  return a.jid
-}
+  if (property) return (a, i, b) =
