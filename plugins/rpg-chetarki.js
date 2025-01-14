fd0ba5ba6608a137622a86a3e8a17301295ce6e2
ZxYerSixZx <oroscoyerson2019@gmail.com>
2025-01-14 18:46:46 -0500

    Actualizar rpg-chetarki.js

--- plugins/rpg-chetarki.js
+++ plugins/rpg-chetarki.js
@@ -1,13 +1,21 @@
-let handler = async (m, { conn }) => {
-    let users = global.db.data.users;
-    users[m.sender].limit = Infinity; 
+let handler = async (m, { conn, isOwner }) => {
+    if (!isOwner) {
+        return m.reply('❌ Este comando solo está disponible para los propietarios.');
+    }
 
-    await m.reply('¡Ahora tienes *KI infinito*! 🎉');
+    let users = global.db.data.users;
+    if (users[m.sender]) {
+        users[m.sender].limit = Infinity;
+        await m.reply('¡Ahora tienes *KI infinito*! 🎉');
+    } else {
+        await m.reply('❌ No se pudo encontrar al usuario en la base de datos.');
+    }
 };
 
 handler.help = ['chetarki'];
 handler.tags = ['rpg'];
 handler.command = ['chetarki'];
 handler.register = true;
-handler.owner = true
+handler.owner = true;
+
 export default handler;
